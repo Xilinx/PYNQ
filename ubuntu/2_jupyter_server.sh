420fe8f75a7d1f09447195a6e97cebbd23e99363
schelleg <schelleg@users.noreply.github.com>
2016-03-31 10:47:47 -0600

    new call to notebook for cleaner jupyter terminal

--- ubuntu/2_jupyter_server.sh
+++ ubuntu/2_jupyter_server.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-
-jupyter notebook 2> /root/jupyter.log &
+su -c "jupyter notebook 2> /root/jupyter.log &" -s /bin/bash root
 
 script_name=`readlink -f "$0"`
 
 echo "${script_name}: Jupyter server started"
-
-
