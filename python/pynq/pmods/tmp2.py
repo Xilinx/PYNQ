66664c42cc88c72db64c6d924143c5237a4f6783
Yun Qu <yunq@xilinx.com>
2016-04-11 14:21:37 -0700

    comment fix

--- python/pynq/pmods/tmp2.py
+++ python/pynq/pmods/tmp2.py
@@ -166,12 +166,12 @@ class TMP2(object):
         #: First stop logging
         self.stop_log()
 
-        # prep iterators and results list
+        #: Prep iterators and results list
         head_ptr = self.mmio.read(pmod_const.MAILBOX_OFFSET+0x8)
         tail_ptr = self.mmio.read(pmod_const.MAILBOX_OFFSET+0xC)
         temps = list()
 
-        # sweep circular buffer for samples
+        #: Sweep circular buffer for samples
         if head_ptr == tail_ptr:
             return None
         elif head_ptr < tail_ptr:
