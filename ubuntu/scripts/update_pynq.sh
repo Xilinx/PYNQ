2bd32f61da3cf267189c9abcf6a747ec539dc347
schelleg <schelleg@users.noreply.github.com>
2016-04-29 14:28:26 -0600

    Update update_pynq.sh

--- ubuntu/scripts/update_pynq.sh
+++ ubuntu/scripts/update_pynq.sh
@@ -48,7 +48,7 @@ echo "5. Transfer Git files into final filesystem locations with correct ownersh
 rm -rf $FINAL_DOCS_DIR/* $FINAL_NOTEBOOKS_DIR/* 
 cp -r $REPO_DIR/docs/build/html/* $FINAL_DOCS_DIR
 cp -r $REPO_DIR/python/notebooks/* $FINAL_NOTEBOOKS_DIR
-cp -r $REPO_DIR/ubuntu/*.sh $FINAL_SCRIPTS_DIR
+cp -r $REPO_DIR/ubuntu/scripts/hostname.sh $FINAL_SCRIPTS_DIR
 
 # 5a. (PLACEHOLDER) Jupyer_notebooks/Getting Started derived contents
 rm -rf $FINAL_NOTEBOOKS_DIR/Getting_Started/*
