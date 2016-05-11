316978ea1c76ee34a816ca5f4677f3f7ffb88c40
Graham Schelle <graham.schelle@gmail.com>
2016-05-11 16:42:15 -0700

    Fixing issue #71.  mailbox can be imported if moved out to temporary location

--- zybo/sdk/build_xsdk.tcl
+++ zybo/sdk/build_xsdk.tcl
@@ -10,7 +10,15 @@ if {![file exists "bsp"]} {
     sdk create_bsp_project -name bsp -hwproject hw_def -proc iop1_mb -os standalone
 }
 
-sdk build all
+
+if {![file exists "sdk_import"]} {
+   file mkdir sdk_import
+   file copy -force mailbox sdk_import
+}
+
+sdk import_project sdk_import/mailbox
+
+#sdk build all
 
 puts "To use SDK, from this folder execute"
 puts "    xsdk -workspace ."
