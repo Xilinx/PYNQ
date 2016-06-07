f3a98ca29d3ef2f9ae2fd9c75da5d8500e68bea1
hackwa <anurag6dubey@gmail.com>
2016-06-07 08:40:40 -0600

    add script to resize the filesystem at boot time

---
+++ ubuntu/4_resizefs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# This script is used to resize the ubuntu image to fill up the entire
+# space available in the SDCard. Run as root if running manually.
+# The sed script strips off all the comments so that we can 
+# document what we're doing in-line with the actual commands
+# Note that a blank line (commented as "defualt" will send a empty
+# line terminated with a newline to take the fdisk default.
+
+TGTDEV=/dev/mmcblk0
+TGTPART=/dev/mmcblk0p2
+
+if [[ ${RESIZED} == "1" ]]; then
+	echo "filesystem already resized!"
+	exit 0
+fi
+
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV} 2>/dev/null
+	p # print the in-memory partition table
+	d # delete
+	2 # second partition
+	n #  new partition
+	p # primary partition
+	2 # second partition
+	  # default first sector
+	  # fill up the entire space
+	p # print the new partition table
+  	w # and we're done
+EOF
+
+partx -u ${TGTPART}
+resize2fs ${TGTPART}
+echo "RESIZED=1" | tee -a /etc/environment
+echo "Done!"
