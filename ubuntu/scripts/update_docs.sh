bb161c13253891b2ba603206b1651be87520f064
Graham Schelle <graham.schelle@gmail.com>
2016-07-29 14:58:49 -0700

    updated update scripts and removed iop project

--- ubuntu/scripts/update_docs.sh
+++ ubuntu/scripts/update_docs.sh
@@ -8,29 +8,6 @@ FINAL_DOCS_DIR=/home/xpp/docs
 FINAL_NOTEBOOKS_DIR=/home/xpp/jupyter_notebooks
 FINAL_SCRIPTS_DIR=/home/xpp/scripts
 
-GS_NOTEBOOK_IMAGES="edit_mode.png
-dashboard_running_tab.png
-python_logo.svg
-dashboard_files_tab_run.png
-zybo_io_opt.jpeg
-command_mode.png
-zybo_io.jpeg
-dashboard_files_tab_new.png
-dashboard_files_tab.png
-menubar_toolbar.png
-dashboard_files_tab_btns.png
-zyboaudiovideo.jpg
-Pmods_opt.png
-pmod_closeup_opt.jpeg
-pmod_pins_opt.png
-Pmod_Grove_Adapter_opt.jpeg
-tmp2_8pin_opt.jpeg
-iop.jpg
-zybopmods_opt.jpeg
-pmodio_overlay_1_opt.png
-als_oled_Demo_opt.jpeg
-adc_dac_demo_opt.jpeg"
-
 
 if ! [ $(id -u) = 0 ]; then
    echo "to be run with sudo"
@@ -45,59 +22,11 @@ if [ -d $REPO_DIR ] || [ -d $BACKUP_DIR ] ; then
    exit 1
 fi
 
-echo "1. Backing up files into ${BACKUP_DIR}"
-mkdir $BACKUP_DIR
-cp -r $FINAL_DOCS_DIR $FINAL_NOTEBOOKS_DIR $FINAL_SCRIPTS_DIR $BACKUP_DIR
-
-echo "2. Build docs"
-git clone https://github.com/Xilinx/PYNQ $REPO_DIR
-cd $REPO_DIR/docs
-sphinx-apidoc -f -o ./source $PYNQ_DIR
-python3 ipynb_post_processor.py
-make clean ; make html
-
-echo "3. Transfer Git files into final filesystem with correct ownership"
-rm -rf $FINAL_DOCS_DIR/* $FINAL_NOTEBOOKS_DIR/* 
-cp -r $REPO_DIR/docs/build/html/* $FINAL_DOCS_DIR
-cp -r $REPO_DIR/$BOARD/notebooks/* $FINAL_NOTEBOOKS_DIR
-cp -r $REPO_DIR/ubuntu/scripts/hostname.sh $FINAL_SCRIPTS_DIR
-cp -r $REPO_DIR/ubuntu/scripts/*.py $FINAL_SCRIPTS_DIR
-pushd $FINAL_NOTEBOOKS_DIR ; ln -s $FINAL_DOCS_DIR ; popd
-pushd $REPO_DIR/docs/source/temp
-for f in *.tmp
-do 
-    mv -- "$f" "$REPO_DIR/docs/source/${f%.tmp}.ipynb"
-done
-popd
-rm -rf $REPO_DIR/docs/source/temp
-mkdir $FINAL_NOTEBOOKS_DIR/Getting_Started
-cp $REPO_DIR/docs/source/3_jupyter_notebook.ipynb \
-    $FINAL_NOTEBOOKS_DIR/Getting_Started/1_jupyter_notebook.ipynb
-cp $REPO_DIR/docs/source/4_programming_in_python.ipynb \
-    $FINAL_NOTEBOOKS_DIR/Getting_Started/2_programming_in_python.ipynb
-cp $REPO_DIR/docs/source/5_programming_onboard_peripherals.ipynb \
-    $FINAL_NOTEBOOKS_DIR/Getting_Started/3_programming_onboard_peripherals.ipynb
-cp $REPO_DIR/docs/source/6_pmodio_overlay.ipynb \
-    $FINAL_NOTEBOOKS_DIR/Getting_Started/4_pmodio_overlay.ipynb
-cp $REPO_DIR/docs/source/7_audio_video_overlay.ipynb \
-    $FINAL_NOTEBOOKS_DIR/Getting_Started/5_audio_video_overlay.ipynb
-
-mkdir $FINAL_NOTEBOOKS_DIR/Getting_Started/images
-for f in $GS_NOTEBOOK_IMAGES
-do
-    cp $REPO_DIR/docs/source/images/$f $FINAL_NOTEBOOKS_DIR/Getting_Started/images/
-done
+make -f makefile.pynq update_docs
 
 
-chmod -R a+rw $FINAL_NOTEBOOKS_DIR $FINAL_DOCS_DIR $PYNQ_DIR
-chmod -R a+x $FINAL_SCRIPTS_DIR/*
-chown -R xpp:xpp $REPO_DIR $BACKUP_DIR
-chown -R xpp:xpp $FINAL_NOTEBOOKS_DIR $FINAL_DOCS_DIR $FINAL_SCRIPTS_DIR $PYNQ_DIR 
 
-echo ""
-echo "Completed build."
-echo "Documentation folder is at: $FINAL_DOCS_DIR"
-echo ""
-echo "To update this file, manually replace it from local git:"
-echo "cp -r $REPO_DIR/ubuntu/scripts/$SCRIPT_NAME $FINAL_SCRIPTS_DIR"
-echo ""
+
+
+
+
--- ubuntu/scripts/update_pynq.sh
+++ ubuntu/scripts/update_pynq.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-SCRIPT_NAME=`basename "$0"`
 BACKUP_DIR=/home/xpp/pynq_backup
 REPO_DIR=/home/xpp/pynq_git
-PYNQ_DIR=/usr/local/lib/python3.4/dist-packages/pynq
-
-FINAL_DOCS_DIR=/home/xpp/docs
-FINAL_NOTEBOOKS_DIR=/home/xpp/jupyter_notebooks
-FINAL_SCRIPTS_DIR=/home/xpp/scripts
-
 
 if ! [ $(id -u) = 0 ]; then
    echo "to be run with sudo"
@@ -23,33 +16,4 @@ if [ -d $REPO_DIR ] || [ -d $BACKUP_DIR ] ; then
    exit 1
 fi
 
-echo "1. Backing up files into ${BACKUP_DIR}"
-mkdir $BACKUP_DIR
-cp -r $FINAL_DOCS_DIR $FINAL_NOTEBOOKS_DIR $FINAL_SCRIPTS_DIR $BACKUP_DIR
-rm -rf $FINAL_NOTEBOOKS_DIR/* 
-python3.4 /home/xpp/scripts/stop_pl_server.py &
-
-echo "2. Clone PYNQ repository into ${REPO_DIR}"
-git clone https://github.com/Xilinx/PYNQ $REPO_DIR
-
-echo "3. Pip install latest pynq python package"
-rm -rf $PYNQ_DIR/*
-cp -rf $REPO_DIR/$BOARD/sdk/bin/*.bin $REPO_DIR/python/pynq/iop/
-cp -rf $REPO_DIR/$BOARD/bitstream $REPO_DIR/python/pynq/
-cd $REPO_DIR/python
-sudo -H pip install --upgrade .
-python3.4 /home/xpp/scripts/start_pl_server.py &
-chown -R xpp:xpp $PYNQ_DIR/*
-
-echo "4. Update scripts and notebooks"
-cp -r $REPO_DIR/$BOARD/notebooks/* $FINAL_NOTEBOOKS_DIR
-cp -r $REPO_DIR/ubuntu/scripts/hostname.sh $FINAL_SCRIPTS_DIR
-cp -r $REPO_DIR/ubuntu/scripts/*.py $FINAL_SCRIPTS_DIR
-
-echo "Notebooks     folder is at: $FINAL_NOTEBOOKS_DIR"
-echo "Scripts       folder is at: $FINAL_SCRIPTS_DIR"
-echo "To update this file, manually replace it from local git:"
-echo "cp -r $REPO_DIR/ubuntu/scripts/$SCRIPT_NAME $FINAL_SCRIPTS_DIR"
-echo ""
-echo "Completed build."
-echo ""
+make -f makefile.pynq update_all
