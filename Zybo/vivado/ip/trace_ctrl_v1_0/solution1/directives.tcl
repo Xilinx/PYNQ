d4fb444fa3fd6356746c290daed34315d852a5cb
Yun Qu <yunq@xilinx.com>
2016-08-31 10:20:12 -0700

    retire zybo folder

--- Zybo/vivado/ip/trace_ctrl_v1_0/solution1/directives.tcl
+++
@@ -1,5 +0,0 @@
-############################################################
-## This file is generated automatically by Vivado HLS.
-## Please DO NOT edit it.
-## Copyright (C) 2015 Xilinx Inc. All rights reserved.
-############################################################
