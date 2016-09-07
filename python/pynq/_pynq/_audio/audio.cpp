571b6ccb19725478c1957e3186fedad3fb3c8eaa
Benedikt Janssen <benedikt@xilinx.com>
2016-09-07 07:18:09 -0600

    Audio header file added + reformated

--- python/pynq/_pynq/_audio/audio.cpp
+++ python/pynq/_pynq/_audio/audio.cpp
@@ -1,3 +1,21 @@
+/******************************************************************************
+ * @file audio.c
+ *
+ * Functions to control audio controller.
+ *
+ * <pre>
+ * MODIFICATION HISTORY:
+ *
+ * Ver   Who          Date     Changes
+ * ----- ------------ -------- -----------------------------------------------
+ * 1.00a Mihaita Nagy 04/06/12 First release
+ * 1.00b beja         08/15/16 Rewritten for Pynq-Z1
+ * 1.00c beja         09/07/16 Header added
+ *
+ * </pre>
+ *
+ *****************************************************************************/
+
 #include "xil_io.h"
 #include "xil_types.h"
 
@@ -15,8 +33,8 @@ void Xil_Out32(UINTPTR Addr, u32 Value)
 
 
 /*****************************************************************************/
-
-extern "C" void _Pynq_record(unsigned int BaseAddr, unsigned int * BufAddr, unsigned int nsamples)
+extern "C" void _Pynq_record(unsigned int BaseAddr, unsigned int * BufAddr, 
+							 unsigned int nsamples)
 {
 	//Audio controller registers
 	unsigned int PDM_RESET_REG				= 0x00;
@@ -25,28 +43,29 @@ extern "C" void _Pynq_record(unsigned int BaseAddr, unsigned int * BufAddr, unsi
 	unsigned int PDM_DATA_IN_REG         	= 0x0c;
 	unsigned int PDM_DATA_OUT_REG          	= 0x10;
 	unsigned int PDM_STATUS_REG           	= 0x14;
-	// Audio controller Status Register Flags
+	//Audio controller Status Register Flags
 	unsigned int TX_FIFO_EMPTY				= 0;
 	unsigned int TX_FIFO_FULL				= 1;
 	unsigned int RX_FIFO_EMPTY				= 16;
 	unsigned int RX_FIFO_FULL				= 17;
 
-	//...
+	//Auxiliary
 	unsigned long u32Temp, i=0;
 
-	//reset pdm
+	//Reset pdm
 	Xil_Out32(BaseAddr + PDM_RESET_REG, 0x01);
 	Xil_Out32(BaseAddr + PDM_RESET_REG, 0x00);
-	//reset fifos
+	//Reset fifos
 	Xil_Out32(BaseAddr + PDM_FIFO_CONTROL_REG, 0xC0000000);
 	Xil_Out32(BaseAddr + PDM_FIFO_CONTROL_REG, 0x00000000);
-	//receive
+	//Receive
 	Xil_Out32(BaseAddr + PDM_TRANSFER_CONTROL_REG, 0x00);
 	Xil_Out32(BaseAddr + PDM_TRANSFER_CONTROL_REG, 0x05);
 
-	//sample
+	//Sample
 	while(i < nsamples){
-		u32Temp = ((Xil_In32(BaseAddr + PDM_STATUS_REG)) >> RX_FIFO_EMPTY) & 0x01;
+		u32Temp = ((Xil_In32(BaseAddr + PDM_STATUS_REG)) 
+		           >> RX_FIFO_EMPTY) & 0x01;
 		if(u32Temp == 0){
 			Xil_Out32(BaseAddr + PDM_FIFO_CONTROL_REG, 0x00000002);
 			Xil_Out32(BaseAddr + PDM_FIFO_CONTROL_REG, 0x00000000);
@@ -55,12 +74,13 @@ extern "C" void _Pynq_record(unsigned int BaseAddr, unsigned int * BufAddr, unsi
 		}
 	}
 
-	//stop
+	//Stop
 	Xil_Out32(BaseAddr + PDM_TRANSFER_CONTROL_REG, 0x02);
 }
 
 /*****************************************************************************/
-extern "C" void _Pynq_play(unsigned int BaseAddr, unsigned int * BufAddr, unsigned int nsamples)
+extern "C" void _Pynq_play(unsigned int BaseAddr, unsigned int * BufAddr, 
+						   unsigned int nsamples)
 {
 	//Audio controller registers
 	unsigned int PDM_RESET_REG				= 0x00;
@@ -69,27 +89,28 @@ extern "C" void _Pynq_play(unsigned int BaseAddr, unsigned int * BufAddr, unsign
 	unsigned int PDM_DATA_IN_REG         	= 0x0c;
 	unsigned int PDM_DATA_OUT_REG          	= 0x10;
 	unsigned int PDM_STATUS_REG           	= 0x14;
-	// Audio controller Status Register Flags
+	//Audio controller Status Register Flags
 	unsigned int TX_FIFO_EMPTY				= 0;
 	unsigned int TX_FIFO_FULL				= 1;
 	unsigned int RX_FIFO_EMPTY				= 16;
 	unsigned int RX_FIFO_FULL				= 17;
 
-	//...
+	//Auxiliary
 	unsigned long u32Temp, u32DWrite, i=0;
-	//reset i2s
+	//Reset i2s
 	Xil_Out32(BaseAddr + PDM_RESET_REG, 0x01);
 	Xil_Out32(BaseAddr + PDM_RESET_REG, 0x00);
-	//reset fifos
+	//Reset fifos
 	Xil_Out32(BaseAddr + PDM_FIFO_CONTROL_REG, 0xC0000000);
 	Xil_Out32(BaseAddr + PDM_FIFO_CONTROL_REG, 0x00000000);
-	//transmit
+	//Transmit
 	Xil_Out32(BaseAddr + PDM_TRANSFER_CONTROL_REG, 0x00);
 	Xil_Out32(BaseAddr + PDM_TRANSFER_CONTROL_REG, 0x09);
 
-	//play
+	//Play
 	while(i < nsamples) {
-		u32Temp = ((Xil_In32(BaseAddr + PDM_STATUS_REG)) >> TX_FIFO_FULL) & 0x01;
+		u32Temp = ((Xil_In32(BaseAddr + PDM_STATUS_REG)) 
+				   >> TX_FIFO_FULL) & 0x01;
 		if(u32Temp == 0) {
 			Xil_Out32(BaseAddr + PDM_DATA_IN_REG, BufAddr[i]);
 			Xil_Out32(BaseAddr + PDM_FIFO_CONTROL_REG, 0x00000001);
@@ -97,6 +118,6 @@ extern "C" void _Pynq_play(unsigned int BaseAddr, unsigned int * BufAddr, unsign
 			i++;
 		}
 	}
-	//stop/reset
+	//Stop/Reset
 	Xil_Out32(BaseAddr + PDM_TRANSFER_CONTROL_REG, 0x00);
 }
