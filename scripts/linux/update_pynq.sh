0f1bf08aaed9405e587e59a4c4813ab29e0c3153
hackwa <anurag.dubey@colorado.edu>
2016-09-06 18:09:12 -0600

    update docs support in pynq_update

--- scripts/linux/update_pynq.sh
+++ scripts/linux/update_pynq.sh
@@ -9,28 +9,32 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 
-usage="Usage : $(basename "$0") [-h] [-l] [-s]
+usage="Usage : $(basename "$0") [-h] [-l] [-s] [-d]
 Update pynq python, notebooks and scripts from PYNQ repository
 
 where:
     -h  show this help text
     -l  update packages to latest branch
 	Note: This could result in an unstable build
-    -s  update packages to latest stable release"
+    -s  update packages to latest stable release
+    -d  rebuild docs from source"
 
-getopts ':hl' option
-  case "$option" in
-    h) echo "$usage"
-       exit
-       ;;
-    l) branch=master
-       echo "Using master branch for update.."
-       ;;
-    s) echo "Updating to latest stable release" 
-       ;;
-   \?) echo "Updating to latest stable release (default action)" 
-       ;;
-esac
+while getopts ':hld' option; do
+    case "$option" in
+        h) echo "$usage"
+           exit
+           ;;
+        l) branch=master
+           echo "Using master branch for update.."
+           ;;
+        s) echo "Updating to latest stable release"
+           ;;
+        d) docs=true
+           ;;
+       \?) echo "Updating to latest stable release (default action)"
+           ;;
+    esac
+done
 
 echo "Info: This operation will overwrite all the example notebooks"
 read -rsp $'Press any key to continue...\n' -n1 key
@@ -57,6 +61,12 @@ fi
 
 make -f ${MAKEFILE_PATH}/makefile.pynq update_pynq
 retval=$?
+
+if [[ -n $docs ]]; then
+    echo "Starting Docs Build"
+    make -f ${MAKEFILE_PATH}/makefile.pynq update_docs
+fi
+
 echo "Cleaning up.."
 cd ${REPO_DIR}
 git checkout -q master
