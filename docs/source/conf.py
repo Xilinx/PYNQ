39e3e71fe65902a7e13025409d087573fca9c966
plysaght <plysaght@xilinx.com>
2016-09-23 13:03:58 -0700

    exclude_patterns = ['*pl*']

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,7 +83,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['pl.py']
+exclude_patterns = ['*pl*']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
