4501fdf3e8fa37d73dcb5b9cd23ba097ee4d5cab
plysaght <plysaght@xilinx.com>
2016-09-23 13:11:49 -0700

    exclude ['../../python/pynq/pl.py']

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,7 +83,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['*pl*']
+exclude_patterns = ['../../python/pynq/pl.py']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
