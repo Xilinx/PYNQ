f1ea06541dcd8c7acc19b060f36f4a9e7ff485bc
root <yunq@xilinx.com>
2017-06-26 17:48:44 +0000

    fix __init__.py

--- pynq/lib/__init__.py
+++ pynq/lib/__init__.py
@@ -27,9 +27,6 @@
 #   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
 #   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__author__ = "Graham Schelle"
-__copyright__ = "Copyright 2016, Xilinx"
-__email__ = "pynq_support@xilinx.com"
 
 # from .audio import Audio
 # from .video import HDMI
@@ -38,13 +35,70 @@ __email__ = "pynq_support@xilinx.com"
 # from .trace_buffer import Trace_Buffer
 # from .usb_wifi import Usb_Wifi
 
+from .pynqmicroblaze import PynqMicroblaze
 from .led import LED
 from .rgbled import RGBLED
 from .switch import Switch
 from .button import Button
 
-from .intf import request_intf
-from .intf import FSMBuilder
-from .intf import PatternBuilder
-from .intf import BooleanBuilder
-from .intf import Waveform
+from .arduino import Arduino
+from .arduino import Arduino_DevMode
+from .arduino import Arduino_IO
+from .arduino import Arduino_Analog
+from .arduino import Arduino_LCD18
+from .arduino import Grove_PIR
+from .arduino import Grove_ADC
+from .arduino import Grove_OLED
+from .arduino import Grove_IMU
+from .arduino import Grove_LEDbar
+from .arduino import Grove_TMP
+from .arduino import Grove_Light
+from .arduino import Grove_Buzzer
+from .arduino import Grove_Color
+from .arduino import Grove_Dlight
+from .arduino import Grove_EarHR
+from .arduino import Grove_FingerHR
+from .arduino import Grove_HapticMotor
+from .arduino import Grove_TH02
+
+from .pmod import Pmod
+from .pmod import Pmod_DevMode
+from .pmod import Pmod_ADC
+from .pmod import Pmod_DAC
+from .pmod import Pmod_OLED
+from .pmod import Pmod_LED8
+from .pmod import Pmod_IO
+from .pmod import Pmod_IIC
+from .pmod import Pmod_DPOT
+from .pmod import Pmod_TC1
+from .pmod import Pmod_TMP2
+from .pmod import Pmod_ALS
+from .pmod import Pmod_Cable
+from .pmod import Pmod_Timer
+from .pmod import Pmod_PWM
+from .pmod import Grove_PIR
+from .pmod import Grove_ADC
+from .pmod import Grove_OLED
+from .pmod import Grove_IMU
+from .pmod import Grove_LEDbar
+from .pmod import Grove_TMP
+from .pmod import Grove_Light
+from .pmod import Grove_Buzzer
+from .pmod import Grove_Color
+from .pmod import Grove_Dlight
+from .pmod import Grove_EarHR
+from .pmod import Grove_FingerHR
+from .pmod import Grove_HapticMotor
+from .pmod import Grove_TH02
+
+# from .intf import Intf
+# from .intf import Waveform
+# from .intf import BooleanBuilder
+# from .intf import PatternBuilder
+# from .intf import TraceAnalyzer
+# from .intf import FSMBuilder
+
+
+__author__ = "Graham Schelle"
+__copyright__ = "Copyright 2016, Xilinx"
+__email__ = "pynq_support@xilinx.com"
