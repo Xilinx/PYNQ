bbbc21c24b60299fafeb319d5f7790bdb690d09f
Dustin Richmond <drichmond@eng.ucsd.edu>
2017-06-26 14:39:25 -0600

    Refactoring xlnk.py and dma.py to add CPU_ARCH_IS_SUPPORTED guard to class scopes
    * Moved guard from global scope to class scope
    * Moved root check from global to class scope
    * Moved global variables from global to class scope

--- pynq/lib/dma.py
+++ pynq/lib/dma.py
@@ -34,136 +34,13 @@ import cffi
 import functools
 import signal
 import numpy as np
+import warnings
+from pynq.ps import CPU_ARCH_IS_SUPPORTED, CPU_ARCH
 
 
 __author__ = 'Peter Ogden, Anurag Dubey'
 __copyright__ = 'Copyright 2017, Xilinx'
 __email__ = 'pynq_support@xilinx.com'
-
-
-ffi = cffi.FFI()
-memapi = cffi.FFI()
-
-ffi.cdef("""
-typedef unsigned int* XAxiDma_Bd[20];
-typedef struct {
-        uint32_t ChanBase;           /**< physical base address*/
-        int IsRxChannel;        /**< Is this a receive channel */
-        volatile int RunState;  /**< Whether channel is running */
-        int HasStsCntrlStrm;    /**< Whether has stscntrl stream */
-        int HasDRE;
-        int DataWidth;
-        int Addr_ext;
-        uint32_t MaxTransferLen;
-
-        uint32_t * FirstBdPhysAddr; /**< Physical address of 1st BD in list */
-        uint32_t * FirstBdAddr;  /**< Virtual address of 1st BD in list */
-        uint32_t * LastBdAddr;  /**< Virtual address of last BD in the list */
-        uint32_t Length;         /**< Total size of ring in bytes */
-        uint32_t * Separation;  /**< Number of bytes between the starting
-                                     address of adjacent BDs */
-        XAxiDma_Bd *FreeHead;   /**< First BD in the free group */
-        XAxiDma_Bd *PreHead;    /**< First BD in the pre-work group */
-        XAxiDma_Bd *HwHead;     /**< First BD in the work group */
-        XAxiDma_Bd *HwTail;     /**< Last BD in the work group */
-        XAxiDma_Bd *PostHead;   /**< First BD in the post-work group */
-        XAxiDma_Bd *BdaRestart; /**< BD to load when channel is started */
-        int FreeCnt;            /**< Number of allocatable BDs in free group */
-        int PreCnt;             /**< Number of BDs in pre-work group */
-        int HwCnt;              /**< Number of BDs in work group */
-        int PostCnt;            /**< Number of BDs in post-work group */
-        int AllCnt;             /**< Total Number of BDs for channel */
-        int RingIndex;          /**< Ring Index */
-} XAxiDma_BdRing;
-typedef struct {
-        uint32_t DeviceId;
-        uint32_t * BaseAddr;
-
-        int HasStsCntrlStrm;
-        int HasMm2S;
-        int HasMm2SDRE;
-        int Mm2SDataWidth;
-        int HasS2Mm;
-        int HasS2MmDRE;
-        int S2MmDataWidth;
-        int HasSg;
-        int Mm2sNumChannels;
-        int S2MmNumChannels;
-        int Mm2SBurstSize;
-        int S2MmBurstSize;
-        int MicroDmaMode;
-        int AddrWidth;            /**< Address Width */
-} XAxiDma_Config;
-typedef struct XAxiDma {
-        uint32_t RegBase;            /* Virtual base address of DMA engine */
-        int HasMm2S;            /* Has transmit channel */
-        int HasS2Mm;            /* Has receive channel */
-        int Initialized;        /* Driver has been initialized */
-        int HasSg;
-        XAxiDma_BdRing TxBdRing;     /* BD container management */
-        XAxiDma_BdRing RxBdRing[16]; /* BD container management */
-        int TxNumChannels;
-        int RxNumChannels;
-        int MicroDmaMode;
-        int AddrWidth;            /**< Address Width */
-} XAxiDma;
-unsigned int getMemoryMap(unsigned int phyAddr, unsigned int len);
-unsigned int getPhyAddr(void *buf);
-void frame_free(void *buf);
-int XAxiDma_CfgInitialize(XAxiDma * InstancePtr, XAxiDma_Config *Config);
-void XAxiDma_Reset(XAxiDma * InstancePtr);
-int XAxiDma_ResetIsDone(XAxiDma * InstancePtr);
-int XAxiDma_Pause(XAxiDma * InstancePtr);
-int XAxiDma_Resume(XAxiDma * InstancePtr);
-uint32_t XAxiDma_Busy(XAxiDma *InstancePtr,int Direction);
-uint32_t XAxiDma_SimpleTransfer(XAxiDma *InstancePtr,\
-uint32_t * BuffAddr, uint32_t Length,int Direction);
-int XAxiDma_SelectKeyHole(XAxiDma *InstancePtr, int Direction, int Select);
-int XAxiDma_SelectCyclicMode(XAxiDma *InstancePtr, int Direction, int Select);
-int XAxiDma_Selftest(XAxiDma * InstancePtr);
-void DisableInterruptsAll(XAxiDma * InstancePtr);
-""")
-
-memapi.cdef("""
-static uint32_t xlnkBufCnt = 0;
-uint32_t cma_mmap(uint32_t phyAddr, uint32_t len);
-uint32_t cma_munmap(void *buf, uint32_t len);
-void *cma_alloc(uint32_t len, uint32_t cacheable);
-uint32_t cma_get_phy_addr(void *buf);
-void cma_free(void *buf);
-uint32_t cma_pages_available();
-""")
-
-LIB_SEARCH_PATH = os.path.dirname(os.path.realpath(__file__))
-libdma = ffi.dlopen(LIB_SEARCH_PATH + "/libdma.so")
-libxlnk = memapi.dlopen("/usr/lib/libsds_lib.so")
-
-DefaultConfig = {
-    'DeviceId': 0,
-    'BaseAddr': ffi.cast("uint32_t *", 0x00000000),
-    'HasStsCntrlStrm': 0,
-    'HasMm2S': 0,
-    'HasMm2SDRE': 0,
-    'Mm2SDataWidth': 32,
-    'HasS2Mm': 1,
-    'HasS2MmDRE': 0,
-    'S2MmDataWidth': 64,
-    'HasSg': 0,
-    'Mm2sNumChannels': 1,
-    'S2MmNumChannels': 1,
-    'Mm2SBurstSize': 16,
-    'S2MmBurstSize': 64,
-    'MicroDmaMode': 0,
-    'AddrWidth': 32
-}
-
-DMA_TO_DEV = 0
-DMA_FROM_DEV = 1
-DMA_BIDIRECTIONAL = 3
-
-DeviceId = 0
-DMA_TRANSFER_LIMIT_BYTES = 8388607
-
 
 class timeout:
     """Internal timeout functions.
@@ -185,7 +62,6 @@ class timeout:
 
     def __exit__(self, type, value, traceback):
         signal.alarm(0)
-
 
 class LegacyDMA:
     """Python class which controls DMA.
@@ -217,7 +93,146 @@ class LegacyDMA:
     Configuration : dict
         Current DMAinstance configuration values.
 
+    Note
+    ----
+    If this class is parsed on an unsupported architecture it will issue
+    a warning and leave the class variables libxlnk and libdma undefined
+
     """
+
+    ffi = cffi.FFI()
+    ffi.cdef("""
+    typedef unsigned int* XAxiDma_Bd[20];
+
+    typedef struct {
+        uint32_t ChanBase;           /**< physical base address*/
+        int IsRxChannel;        /**< Is this a receive channel */
+        volatile int RunState;  /**< Whether channel is running */
+        int HasStsCntrlStrm;    /**< Whether has stscntrl stream */
+        int HasDRE;
+        int DataWidth;
+        int Addr_ext;
+        uint32_t MaxTransferLen;
+
+        uint32_t * FirstBdPhysAddr; /**< Physical address of 1st BD in list */
+        uint32_t * FirstBdAddr;  /**< Virtual address of 1st BD in list */
+        uint32_t * LastBdAddr;  /**< Virtual address of last BD in the list */
+        uint32_t Length;         /**< Total size of ring in bytes */
+        uint32_t * Separation;  /**< Number of bytes between the starting
+                                     address of adjacent BDs */
+        XAxiDma_Bd *FreeHead;   /**< First BD in the free group */
+        XAxiDma_Bd *PreHead;    /**< First BD in the pre-work group */
+        XAxiDma_Bd *HwHead;     /**< First BD in the work group */
+        XAxiDma_Bd *HwTail;     /**< Last BD in the work group */
+        XAxiDma_Bd *PostHead;   /**< First BD in the post-work group */
+        XAxiDma_Bd *BdaRestart; /**< BD to load when channel is started */
+        int FreeCnt;            /**< Number of allocatable BDs in free group */
+        int PreCnt;             /**< Number of BDs in pre-work group */
+        int HwCnt;              /**< Number of BDs in work group */
+        int PostCnt;            /**< Number of BDs in post-work group */
+        int AllCnt;             /**< Total Number of BDs for channel */
+        int RingIndex;          /**< Ring Index */
+    } XAxiDma_BdRing;
+
+    typedef struct {
+        uint32_t DeviceId;
+        uint32_t * BaseAddr;
+
+        int HasStsCntrlStrm;
+        int HasMm2S;
+        int HasMm2SDRE;
+        int Mm2SDataWidth;
+        int HasS2Mm;
+        int HasS2MmDRE;
+        int S2MmDataWidth;
+        int HasSg;
+        int Mm2sNumChannels;
+        int S2MmNumChannels;
+        int Mm2SBurstSize;
+        int S2MmBurstSize;
+        int MicroDmaMode;
+        int AddrWidth;            /**< Address Width */
+    } XAxiDma_Config;
+
+    typedef struct XAxiDma {
+        uint32_t RegBase;            /* Virtual base address of DMA engine */
+        int HasMm2S;            /* Has transmit channel */
+        int HasS2Mm;            /* Has receive channel */
+        int Initialized;        /* Driver has been initialized */
+        int HasSg;
+        XAxiDma_BdRing TxBdRing;     /* BD container management */
+        XAxiDma_BdRing RxBdRing[16]; /* BD container management */
+        int TxNumChannels;
+        int RxNumChannels;
+        int MicroDmaMode;
+        int AddrWidth;            /**< Address Width */
+    } XAxiDma;
+
+    unsigned int getMemoryMap(unsigned int phyAddr, unsigned int len);
+    unsigned int getPhyAddr(void *buf);
+    void frame_free(void *buf);
+    int XAxiDma_CfgInitialize(XAxiDma * InstancePtr, XAxiDma_Config *Config);
+    void XAxiDma_Reset(XAxiDma * InstancePtr);
+    int XAxiDma_ResetIsDone(XAxiDma * InstancePtr);
+    int XAxiDma_Pause(XAxiDma * InstancePtr);
+    int XAxiDma_Resume(XAxiDma * InstancePtr);
+    uint32_t XAxiDma_Busy(XAxiDma *InstancePtr,int Direction);
+    uint32_t XAxiDma_SimpleTransfer(XAxiDma *InstancePtr,\
+    uint32_t * BuffAddr, uint32_t Length,int Direction);
+    int XAxiDma_SelectKeyHole(XAxiDma *InstancePtr, int Direction, int Select);
+    int XAxiDma_SelectCyclicMode(XAxiDma *InstancePtr, int Direction, int Select);
+    int XAxiDma_Selftest(XAxiDma * InstancePtr);
+    void DisableInterruptsAll(XAxiDma * InstancePtr);
+    """)
+    LIB_SEARCH_PATH = os.path.dirname(os.path.realpath(__file__))
+    if CPU_ARCH_IS_SUPPORTED:
+        libdma = ffi.dlopen(LIB_SEARCH_PATH + "/libdma.so")
+    else:
+        warnings.warn("Pynq does not support the CPU Architecture: {}"
+                      .format(CPU_ARCH), ResourceWarning)
+        
+    DMA_TO_DEV = 0
+    DMA_FROM_DEV = 1
+    DMA_BIDIRECTIONAL = 3
+    DMA_TRANSFER_LIMIT_BYTES = 8388607
+
+    DeviceId = 0
+    DefaultConfig = {
+        'DeviceId': 0,
+        'BaseAddr': ffi.cast("uint32_t *", 0x00000000),
+        'HasStsCntrlStrm': 0,
+        'HasMm2S': 0,
+        'HasMm2SDRE': 0,
+        'Mm2SDataWidth': 32,
+        'HasS2Mm': 1,
+        'HasS2MmDRE': 0,
+        'S2MmDataWidth': 64,
+        'HasSg': 0,
+        'Mm2sNumChannels': 1,
+        'S2MmNumChannels': 1,
+        'Mm2SBurstSize': 16,
+        'S2MmBurstSize': 64,
+        'MicroDmaMode': 0,
+        'AddrWidth': 32
+    }
+
+    memapi = cffi.FFI()
+
+    memapi.cdef("""
+    static uint32_t xlnkBufCnt = 0;
+    uint32_t cma_mmap(uint32_t phyAddr, uint32_t len);
+    uint32_t cma_munmap(void *buf, uint32_t len);
+    void *cma_alloc(uint32_t len, uint32_t cacheable);
+    uint32_t cma_get_phy_addr(void *buf);
+    void cma_free(void *buf);
+    uint32_t cma_pages_available();
+    """)
+
+    if CPU_ARCH_IS_SUPPORTED:
+        libxlnk = memapi.dlopen("/usr/lib/libsds_lib.so")
+    else:
+        warnings.warn("Pynq does not support the CPU Architecture: {}"
+                      .format(CPU_ARCH), ResourceWarning)
 
     def __init__(self, address, direction=DMA_FROM_DEV, attr_dict=None):
         """Initializes a new DMA object.
@@ -236,7 +251,7 @@ class LegacyDMA:
 
         The keys in `attr_dict` should exactly match the ones used in default
         config. All the keys are not required. The default configuration is
-        defined in dma.DefaultConfig dict. Users can reinitialize the DMA
+        defined in self.DefaultConfig dict. Users can reinitialize the DMA
         with new configuratiuon after creating the object.
 
         Parameters
@@ -253,16 +268,16 @@ class LegacyDMA:
         self.direction = direction
         self.bufLength = None
         self.phyAddress = address
-        self.DMAengine = ffi.new("XAxiDma *")
-        self.DMAinstance = ffi.new("XAxiDma_Config *")
+        self.DMAengine = self.ffi.new("XAxiDma *")
+        self.DMAinstance = self.ffi.new("XAxiDma_Config *")
         self.Configuration = {}
         self._gen_config(address, direction, attr_dict)
 
-        status = libdma.XAxiDma_CfgInitialize(self.DMAengine, self.DMAinstance)
+        status = self.libdma.XAxiDma_CfgInitialize(self.DMAengine, self.DMAinstance)
         if status != 0:
             raise RuntimeError("Failed to initialize DMA!")
-        libdma.XAxiDma_Reset(self.DMAengine)
-        libdma.DisableInterruptsAll(self.DMAengine)
+        self.libdma.XAxiDma_Reset(self.DMAengine)
+        self.libdma.DisableInterruptsAll(self.DMAengine)
 
     def _gen_config(self, address, direction, attr_dict):
         """Build configuration and map memory.
@@ -271,14 +286,12 @@ class LegacyDMA:
         should not be called by user.
 
         """
-        global DefaultConfig
-        global DeviceId
-        for key in DefaultConfig.keys():
-            self.DMAinstance.__setattr__(key, DefaultConfig[key])
-        if direction == DMA_TO_DEV:
+        for key in self.DefaultConfig.keys():
+            self.DMAinstance.__setattr__(key, self.DefaultConfig[key])
+        if direction == self.DMA_TO_DEV:
             self.DMAinstance.HasS2Mm = 0
             self.DMAinstance.HasMm2S = 1
-        elif direction == DMA_BIDIRECTIONAL:
+        elif direction == self.DMA_BIDIRECTIONAL:
             self.DMAinstance.HasS2Mm = 1
             self.DMAinstance.HasMm2S = 1
         self._bufPtr = None
@@ -290,14 +303,14 @@ class LegacyDMA:
             else:
                 print("Warning: Expecting 3rd Arg to be a dict.")
 
-        virt = libxlnk.cma_mmap(address, 0x10000)
+        virt = self.libxlnk.cma_mmap(address, 0x10000)
         if virt == -1:
             raise RuntimeError("Memory map of driver failed.")
-        self.DMAinstance.BaseAddr = ffi.cast("uint32_t *", virt)
-        self.DMAinstance.DeviceId = DeviceId
-        DeviceId += 1
+        self.DMAinstance.BaseAddr = self.ffi.cast("uint32_t *", virt)
+        self.DMAinstance.DeviceId = self.DeviceId
+        self.DeviceId += 1
 
-        for key in DefaultConfig.keys():
+        for key in self.DefaultConfig.keys():
             self.Configuration[key] = self.DMAinstance.__getattribute__(key)
 
     def __del__(self):
@@ -314,9 +327,9 @@ class LegacyDMA:
         None
 
         """
-        if self.buf is not None and self.buf is not ffi.NULL:
+        if self.buf is not None and self.buf is not self.ffi.NULL:
             self.free_buf()
-        libdma.XAxiDma_Reset(self.DMAengine)
+        self.libdma.XAxiDma_Reset(self.DMAengine)
 
     def transfer(self, num_bytes=-1, direction=-1):
         """Transfer data using DMA (Non-blocking).
@@ -355,14 +368,14 @@ class LegacyDMA:
             direction = self.direction
         if num_bytes > self.bufLength:
             raise RuntimeError("Buffer size smaller than the transfer size")
-        if num_bytes > DMA_TRANSFER_LIMIT_BYTES:
+        if num_bytes > self.DMA_TRANSFER_LIMIT_BYTES:
             raise RuntimeError("DMA transfer size > {}".format(
-                DMA_TRANSFER_LIMIT_BYTES))
-        if direction not in [DMA_FROM_DEV, DMA_TO_DEV]:
+                self.DMA_TRANSFER_LIMIT_BYTES))
+        if direction not in [self.DMA_FROM_DEV, self.DMA_TO_DEV]:
             raise RuntimeError("Invalid direction for transfer.")
         self.direction = direction
         if self.buf is not None:
-            libdma.XAxiDma_SimpleTransfer(
+            self.libdma.XAxiDma_SimpleTransfer(
                 self.DMAengine,
                 self._bufPtr,
                 num_bytes,
@@ -403,14 +416,14 @@ class LegacyDMA:
 
         """
         if self.buf is None:
-            self.buf = libxlnk.cma_alloc(num_bytes, cacheable)
-            if self.buf == ffi.NULL:
+            self.buf = self.libxlnk.cma_alloc(num_bytes, cacheable)
+            if self.buf == self.ffi.NULL:
                 raise RuntimeError("Memory allocation failed.")
         else:
-            libxlnk.cma_free(self.buf)
-            self.buf = libxlnk.cma_alloc(num_bytes, cacheable)
-        bufPhyAddr = libxlnk.cma_get_phy_addr(self.buf)
-        self._bufPtr = ffi.cast("uint32_t *", bufPhyAddr)
+            self.libxlnk.cma_free(self.buf)
+            self.buf = self.libxlnk.cma_alloc(num_bytes, cacheable)
+        bufPhyAddr = self.libxlnk.cma_get_phy_addr(self.buf)
+        self._bufPtr = self.ffi.cast("uint32_t *", bufPhyAddr)
         self.bufLength = num_bytes
 
     def free_buf(self):
@@ -428,9 +441,9 @@ class LegacyDMA:
         None
 
         """
-        if self.buf is None or self.buf == ffi.NULL:
+        if self.buf is None or self.buf == self.ffi.NULL:
             return
-        libxlnk.cma_free(self.buf)
+        self.libxlnk.cma_free(self.buf)
 
     def wait(self, wait_timeout=10):
         """Block till DMA is busy or a timeout occurs.
@@ -452,7 +465,8 @@ class LegacyDMA:
         Error = "DMA wait timed out."
         with timeout(seconds=wait_timeout, error_message=Error):
             while True:
-                if libdma.XAxiDma_Busy(self.DMAengine, self.direction) == 0:
+                if self.libdma.XAxiDma_Busy(self.DMAengine,
+                                            self.direction) == 0:
                     break
 
     def get_buf(self, width=32):
@@ -474,9 +488,9 @@ class LegacyDMA:
         """
         if self.buf is not None:
             if width == 32:
-                return ffi.cast("unsigned int *", self.buf)
+                return self.ffi.cast("unsigned int *", self.buf)
             elif width == 64:
-                return ffi.cast("long long *", self.buf)
+                return self.ffi.cast("long long *", self.buf)
         else:
             raise RuntimeError("Buffer not created.")
 
@@ -505,7 +519,7 @@ class LegacyDMA:
             self.create_buf(totalsize, cacheable)
         if not self.buf:
             raise RuntimeError("Buffer not created or shape not specified")
-        buffer = ffi.buffer(self.buf, self.bufLength)
+        buffer = self.ffi.buffer(self.buf, self.bufLength)
         ret = np.frombuffer(buffer, dtype=dtype)
         ret.shape = shape
         return ret
@@ -519,7 +533,7 @@ class LegacyDMA:
 
         The keys in `attr_dict` should exactly match the ones used in default
         config. All the keys are not required. The default configuration is
-        defined in dma.DefaultConfig dict. Users can reinitialize the DMA
+        defined in self.DefaultConfig dict. Users can reinitialize the DMA
         with new configuratiuon after creating the object.
 
         Parameters
