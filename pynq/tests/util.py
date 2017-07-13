9f7f4e2bde85e1087da3f2c92eb511f53613d224
root <yunq@xilinx.com>
2017-07-13 06:11:36 +0000

    update pytests for basic modules, fix pytests

--- pynq/tests/util.py
+++ pynq/tests/util.py
@@ -27,19 +27,28 @@
 #   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
 #   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__author__      = "Yun Rock Qu"
-__copyright__   = "Copyright 2016, Xilinx"
-__email__       = "pynq_support@xilinx.com"
+__author__ = "Yun Rock Qu"
+__copyright__ = "Copyright 2016, Xilinx"
+__email__ = "pynq_support@xilinx.com"
 
 
 def user_answer_yes(text):
     answer = input(text + ' ([yes]/no)>>> ').lower()
     return answer == 'y' or answer == 'yes' or answer == ''
-    
+
+
 def user_answer_no(text):
     answer = input(text + ' (yes/[no])>>> ').lower()
     return answer == 'n' or answer == 'no' or answer == ''
 
-def get_pmod_id(text):
-    return input("Type in the interface ID of the {} (A or B): "
-                 .format(text)).strip().upper()
+
+def get_interface_id(text, options):
+    options_str = [str(x) for x in options]
+    options_text = '/'.join(options_str)
+    ret_str = input(
+        "Type in the interface ID of the {} ({}): ".format(
+            text, options_text))
+    ret_str = ret_str.strip().upper()
+    if ret_str not in options_str:
+        raise ValueError('Please use a valid interface ID.')
+    return ret_str
