4952a30ca9449c98252cf23f800d1ed6c6076947
Yun Rock Qu <yunq@xilinx.com>
2017-07-21 23:43:19 +0000

    adding sdk and bsp folders

--- pynq/lib/logictools/logictools_controller/Debug/src/subdir.mk
+++ pynq/lib/logictools/logictools_controller/Debug/src/subdir.mk
@@ -7,20 +7,20 @@ LD_SRCS += \
 ../src/lscript.ld 
 
 C_SRCS += \
-../src/arduino_intf.c 
+../src/logictools_controller.c 
 
 OBJS += \
-./src/arduino_intf.o 
+./src/logictools_controller.o 
 
 C_DEPS += \
-./src/arduino_intf.d 
+./src/logictools_controller.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MicroBlaze gcc compiler'
-	mb-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -I../../bsp_intf/iop3_mb/include -mlittle-endian -mcpu=v9.6 -mxl-soft-mul -Wl,--no-relax -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	mb-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -I../../bsp_logictools_controller/lcp_lcp_mb_mb/include -mlittle-endian -mcpu=v9.6 -mxl-soft-mul -Wl,--no-relax -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
