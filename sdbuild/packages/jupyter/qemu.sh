851bc11d4567c8fe5e0baf4f7f632b71625a20e5
Peter Ogden <ogden@xilinx.com>
2017-08-14 14:21:28 +0100

    Increase base jupyter bandwidth

--- sdbuild/packages/jupyter/qemu.sh
+++ sdbuild/packages/jupyter/qemu.sh
@@ -15,6 +15,7 @@ c.NotebookApp.nbserver_extensions = {"jupyterlab":True}
 c.NotebookApp.notebook_dir = '/home/xilinx/jupyter_notebooks'
 c.NotebookApp.password = 'sha1:46c5ef4fa52f:ee46dad5008c6270a52f6272828a51b16336b492'
 c.NotebookApp.port = 9090
+c.NotebookApp.iopub_data_rate_limit = 100000000
 EOT
 
 # Enable widgets
