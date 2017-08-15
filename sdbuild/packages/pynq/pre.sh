e532c6937a8cbb5320b191c7091401ba4de5c6ba
Peter Ogden <ogden@xilinx.com>
2017-08-15 20:50:09 +0100

    Remove reveal.js as RISE installed as an extension (#434)

--- sdbuild/packages/pynq/pre.sh
+++ sdbuild/packages/pynq/pre.sh
@@ -6,7 +6,6 @@ set -e
 target=$1
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo cp -r $WORKDIR/reveal.js $target/root
 sudo cp -r $WORKDIR/PYNQ $target/home/xilinx/pynq_git
 sudo cp $script_dir/pl_server.sh $target/usr/local/bin
 sudo cp $script_dir/pl_server.service $target/lib/systemd/system
