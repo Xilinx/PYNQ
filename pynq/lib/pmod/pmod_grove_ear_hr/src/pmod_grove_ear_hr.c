989ed011a0cfcd80be459cd4902cf2b0c2ebe6e9
Rock Qu <yun.qu@xilinx.com>
2017-09-21 13:26:17 -0700

    remove comments on pynqz1

--- pynq/lib/pmod/pmod_grove_ear_hr/src/pmod_grove_ear_hr.c
+++ pynq/lib/pmod/pmod_grove_ear_hr/src/pmod_grove_ear_hr.c
@@ -34,7 +34,7 @@
  *
  * @file pmod_grove_ear_hr.c
  * IOP code (MicroBlaze) for grove ear-clip heart rate sensor.
- * The sensor has to be connected to a PMOD interface 
+ * The sensor has to be connected to a Pmod interface 
  * via a shield socket.
  * http://wiki.seeed.cc/Grove-Ear-clip_Heart_Rate_Sensor/
  *
@@ -44,7 +44,7 @@
  * Ver   Who  Date     Changes
  * ----- --- -------- -----------------------------------------------
  * 1.00a mr  07/05/16 release
- * 1.00b gn  10/24/16 support for PYNQ-Z1
+ * 1.00b gn  10/24/16 support for Pmod interface. 
  *
  * </pre>
  *
