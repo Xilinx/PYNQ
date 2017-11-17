a1262072b8b56cef460fff77cf0e38b195b534f3
Peter Ogden <ogden@xilinx.com>
2017-11-17 06:24:57 -0500

    Add HTTP server on port 80 that redirects to 9090

--- sdbuild/packages/jupyter/start_jupyter.sh
+++ sdbuild/packages/jupyter/start_jupyter.sh
@@ -4,14 +4,23 @@
 set -a
 . /etc/environment
 set +a
-notebook_args=
+for f in /etc/profile.d/*; do source $f; done
+
+notebook_args=--no-browser
 notebook_version=$(jupyter notebook --version | grep -o '^[0-9]*')
 
 if [ $notebook_version -ge 5 ]; then
-  notebook_args=--allow-root
+  notebook_args="$notebook_args --allow-root"
 fi
 
 cd ~xilinx
 export SHELL=/bin/bash
 jupyter notebook $notebook_args > /var/log/jupyter.log  2>&1 &
 
+while ! wget --no-proxy -q -O - http://localhost:9090/ > /dev/null  ; do
+  echo "Waiting for Jupyter"
+  sleep 1
+done
+
+echo "Starting redirect server"
+redirect_server &
