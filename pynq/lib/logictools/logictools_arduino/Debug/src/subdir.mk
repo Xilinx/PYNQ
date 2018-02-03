ea66d25f996da45d647652be94a94706d7d1c21b
Yun Rock Qu <yunq@xilinx.com>
2018-02-02 16:28:06 -0800

    update logictools sdk project (#532)

--- pynq/lib/logictools/logictools_arduino/Debug/src/subdir.mk
+++ pynq/lib/logictools/logictools_arduino/Debug/src/subdir.mk
@@ -7,20 +7,20 @@ LD_SRCS += \
 ../src/lscript.ld 
 
 C_SRCS += \
-../src/logictools_controller.c 
+../src/logictools_arduino.c 
 
 OBJS += \
-./src/logictools_controller.o 
+./src/logictools_arduino.o 
 
 C_DEPS += \
-./src/logictools_controller.d 
+./src/logictools_arduino.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MicroBlaze gcc compiler'
-	mb-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -I../../bsp_logictools_controller/lcp_lcp_mb_mb/include -mlittle-endian -mcpu=v9.6 -mxl-soft-mul -Wl,--no-relax -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	mb-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -I../../bsp_lcp_ar_mb/lcp_ar_mb/include -mlittle-endian -mcpu=v10.0 -mxl-soft-mul -Wl,--no-relax -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
