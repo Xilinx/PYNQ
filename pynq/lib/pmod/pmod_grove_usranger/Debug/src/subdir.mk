15615802e5368d1006e363b0a925b476911fb5ad
Rock Qu <yunq@xilinx.com>
2018-03-06 13:43:07 -0800

    update pmod_grove_usranger based on new API

--- pynq/lib/pmod/pmod_grove_usranger/Debug/src/subdir.mk
+++ pynq/lib/pmod/pmod_grove_usranger/Debug/src/subdir.mk
@@ -7,20 +7,20 @@ LD_SRCS += \
 ../src/lscript.ld 
 
 C_SRCS += \
-../src/pmod_grove_USranger.c 
+../src/pmod_grove_usranger.c 
 
 OBJS += \
-./src/pmod_grove_USranger.o 
+./src/pmod_grove_usranger.o 
 
 C_DEPS += \
-./src/pmod_grove_USranger.d 
+./src/pmod_grove_usranger.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MicroBlaze gcc compiler'
-	mb-gcc -Wall -O1 -g3 -c -fmessage-length=0 -MT"$@" -I../../bsp_pmod/iop1_mb/include -mlittle-endian -mcpu=v9.5 -mxl-soft-mul -Wl,--no-relax -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	mb-gcc -Wall -O1 -g3 -c -fmessage-length=0 -MT"$@" -I../../bsp_iop_pmod/iop_pmoda_mb/include -mlittle-endian -mcpu=v10.0 -mxl-soft-mul -Wl,--no-relax -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
