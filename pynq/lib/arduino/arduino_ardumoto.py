51f08ff57a58420e4f1a434c0d2da3946a4a048d
Rock Qu <yunq@xilinx.com>
2018-03-06 17:20:39 -0800

    update arduino_ardumoto based on new API

--- pynq/lib/arduino/arduino_ardumoto.py
+++ pynq/lib/arduino/arduino_ardumoto.py
@@ -28,11 +28,7 @@
 #   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-import struct
-from math import ceil
 from . import Arduino
-from . import MAILBOX_OFFSET
-from . import ARDUINO_NUM_ANALOG_PINS
 
 
 __author__ = "Vikhyat Goyal"
@@ -40,30 +36,38 @@ __copyright__ = "Copyright 2016, Xilinx"
 __email__ = "pynq_support@xilinx.com"
 
 
-arduino_joystick_shield_PROGRAM = "arduino_ardumoto.bin"
+ARDUINO_ARDUMOTO_PROGRAM = "arduino_ardumoto.bin"
 CONFIG_IOP_SWITCH = 0x1
-CONFIGURE = 0x3
-RECONFIGURE_DIR = 0x5
+CONFIGURE_PIN = 0x3
+CONFIGURE_POLAR = 0x5
 SET_DIRECTION = 0x7
 SET_SPEED = 0x9
-RUN  = 0xB
+RUN = 0xB
 STOP = 0xD
+
 
 class Arduino_Ardumoto(object):
     """This class controls the Arduino Ardumoto. 
-    
+
+    The Ardumoto shield can be purchased at:
+    https://www.sparkfun.com/products/14180?_ga=2.131154063.1683239815.\
+    1506444499-1487919484.1504886094
+    Ardumoto supports two DC motor driving.
+
+    Hook up instructions can be found at https://learn.sparkfun.com/tutorials/\
+    ardumoto-kit-hookup-guide?_ga=2.147685681.768736901.1523039274-1762361132.\
+    1512080817
+
     Attributes
     ----------
     microblaze : Arduino
         Microblaze processor instance used by this module.
-    log_running : int
-        The state of the log (0: stopped, 1: started).
-    log_interval_ms : int
-        Time in milliseconds between samples on the same channel.
-    gr_pin : list
-        A group of pins on arduino-grove shield.
-    num_channels : int
-        The number of channels sampled.
+    pin_config : int
+        Pin configuration, can be PIN_DEFAULT(0) or PIN_ALTERNATIVE(1).
+    motor_a : dict
+        Dictionary storing the direction and speed information for motor A.
+    motor_b : dict
+        Dictionary storing the direction and speed information for motor B.
 
     """
     def __init__(self, mb_info):
@@ -74,145 +78,184 @@ class Arduino_Ardumoto(object):
         mb_info : dict
             A dictionary storing Microblaze information, such as the
             IP name and the reset name.
-	"""
 
-        self.microblaze = Arduino(mb_info, arduino_joystick_shield_PROGRAM)
-        self.defaultpins = 0
-        self.alternatepins = 1
-        self.pol_default = 0
-        self.pol_reverse = 1
-        self.motorA = 0
-        self.motorB = 1
-        self.forward = 0
-        self.backward = 1
-        # Write configuration and wait for ACK
+        """
+        # constants
+        self.MOTOR_A = 0
+        self.MOTOR_B = 1
+        self.PIN_DEFAULT = 0
+        self.PIN_ALTERNATIVE = 1
+        self.POLAR_DEFAULT = 0
+        self.POLAR_REVERSE = 1
+        self.FORWARD = 0
+        self.BACKWARD = 1
+
+        # instantiation
+        self.microblaze = Arduino(mb_info, ARDUINO_ARDUMOTO_PROGRAM)
+        self.pin_config = self.PIN_DEFAULT
+        self.motor_a = {'status': 'stopped',
+                        'polarity': self.POLAR_DEFAULT,
+                        'direction': self.FORWARD,
+                        'speed': 1}
+        self.motor_b = {'status': 'stopped',
+                        'polarity': self.POLAR_DEFAULT,
+                        'direction': self.FORWARD,
+                        'speed': 1}
 
         self.microblaze.write_blocking_command(CONFIG_IOP_SWITCH)
+        self.configure_pin(self.PIN_DEFAULT)
+        self.configure_polarity(self.MOTOR_A, self.POLAR_DEFAULT)
+        self.configure_polarity(self.MOTOR_B, self.POLAR_DEFAULT)
 
-    def configure_pins(self, pin_comb):
-        """Switch the pins connected to the L298 between two available alternatives:
-	1) pin 2 - Direction control for motor A
-	   pin 3 - PWM control (speed) for motor A
-	   pin 4 - Direction control for motor B
-	   pin 11 - PWM control (speed) for motor B
+    def configure_pin(self, configuration):
+        """Switch the pin configurations.
 
-	2) pin 8 - Direction control for motor A
-	   pin 9 - PWM control (speed) for motor A
-	   pin 7 - Direction control for motor B
-	   pin 10 - PWM control (speed) for motor B
+        This method switch the pin configurations connected to the L298 between
+        two available alternatives:
 
-	Parameters
+        (1) Default:
+        pin 2 - Direction control for motor A;
+        pin 3 - PWM control (speed) for motor A;
+        pin 4 - Direction control for motor B;
+        pin 11 - PWM control (speed) for motor B;
+
+        (2) Alternative:
+        pin 8 - Direction control for motor A;
+        pin 9 - PWM control (speed) for motor A;
+        pin 7 - Direction control for motor B;
+        pin 10 - PWM control (speed) for motor B;
+
+        Parameters
         ----------
-        pin_comb : int
-	0 - defaultpins = Default Configuration (2,3,4,11)
-	1 - alternatepins = Alternate Configuration (8,9,7,10)
-    
-        Returns
-        -------
-        none    
+        configuration : int
+            Can be PIN_DEFAULT(0) or PIN_ALTERNATIVE(1).
+
         """
-        self.microblaze.write_mailbox(0, pin_comb)
-        self.microblaze.write_blocking_command(CONFIGURE)
+        if configuration not in [self.PIN_DEFAULT, self.PIN_ALTERNATIVE]:
+            raise ValueError("Configuration must be {} or {}.".format(
+                self.PIN_DEFAULT, self.PIN_ALTERNATIVE))
+        self.pin_config = configuration
+        self.microblaze.write_mailbox(0, configuration)
+        self.microblaze.write_blocking_command(CONFIGURE_PIN)
 
-    def configure_polarity(self, mot, polarity):
-        """Direction of rotation of motor depends on the polarity connected to the battery.
-	   user should set the Clockwise and counter-clockwise definitions, Depending on how the motor is wired.
+    def configure_polarity(self, motor, polarity):
+        """Set direction of rotation of motor.
 
-	Parameters
+        This may depend on the way the motor is connected to the shield.
+        User should set the clockwise and counter-clockwise definitions,
+        depending on how the motor is wired to the shield.
+
+        Parameters
         ----------
-        sense : int
-	0 - pol_default = FORWARD - Clockwise
-	1 - pol_reverse = REVERSE - Clockwise
-    
-        Returns
-        -------
-        none    
+        motor : int
+            Can be MOTOR_A(0) or MOTOR_B(1).
+        polarity : int
+            Can be POLAR_DEFAULT(0) or POLAR_REVERSE(1).
+
         """
-        self.microblaze.write_mailbox(0, mot)
+        if motor not in [self.MOTOR_A, self.MOTOR_B]:
+            raise ValueError('Motor index can only be {} or {}.'.format(
+                self.MOTOR_A, self.MOTOR_B))
+        if polarity not in [self.POLAR_DEFAULT, self.POLAR_REVERSE]:
+            raise ValueError('Motor polarity can only be {} or {}.'.format(
+                self.POLAR_DEFAULT, self.POLAR_REVERSE))
+        self.microblaze.write_mailbox(0, motor)
         self.microblaze.write_mailbox(0x4, polarity)
-        self.microblaze.write_blocking_command(RECONFIGURE_DIR)
+        self.microblaze.write_blocking_command(CONFIGURE_POLAR)
+        if motor == self.MOTOR_A:
+            self.motor_a['polarity'] = polarity
+        else:
+            self.motor_b['polarity'] = polarity
 
+    def set_direction(self, motor, direction):
+        """Set the direction of rotation for specific motor.
 
-    def set_dir(self, mot, direction):
-        """Set the direction of rotation for specific motor
+        Motor A is the motor connected to pin pair (2,3) or (8,9).
+        Motor B is the motor connected to pin pair (4,11) or (7,10).
 
-	Parameters
+        Parameters
         ----------
-        mot : int
-	0 : motorA- Motor A (connected to (2,3) or (8,9))
-	1 : motorB- Motor B (connected to (4,11) or (7,10))
+        motor : int
+            Can be MOTOR_A(0) or MOTOR_B(1).
+        direction : int
+            Can be FORWARD(0) or BACKWARD(1).
 
-	direction : int
-	0 - forward : Run FORWARD
-	1 - backward : Run REVERSE
-    
-        Returns
-        -------
-        none    
         """
-        self.microblaze.write_mailbox(0, mot)
+        if motor not in [self.MOTOR_A, self.MOTOR_B]:
+            raise ValueError('Motor index can only be {} or {}.'.format(
+                self.MOTOR_A, self.MOTOR_B))
+        if direction not in [self.FORWARD, self.BACKWARD]:
+            raise ValueError('Motor direction can only be {} or {}.'.format(
+                self.FORWARD, self.BACKWARD))
+        self.microblaze.write_mailbox(0, motor)
         self.microblaze.write_mailbox(0x4, direction)
         self.microblaze.write_blocking_command(SET_DIRECTION)
+        if motor == self.MOTOR_A:
+            self.motor_a['direction'] = direction
+        else:
+            self.motor_b['direction'] = direction
 
-        return self.microblaze.read_mailbox(0)
+    def set_speed(self, motor, speed):
+        """Set the speed of rotation for specific motor.
 
-    def set_speed(self, mot, speed):
-        """Set the speed of rotation for specific motor
+        Motor A is the motor connected to pin pair (2,3) or (8,9).
+        Motor B is the motor connected to pin pair (4,11) or (7,10).
 
-	Parameters
+        Parameters
         ----------
-        mot : int
-	0 - Motor A (connected to (2,3) or (8,9))
-	1 - Motor B (connected to (4,11) or (7,10))
+        motor : int
+            Can be MOTOR_A(0) or MOTOR_B(1).
+        speed : int
+            PWM duty cycle for the motor specified, ranging from 1 - 99.
 
-	speed : int
-	PWM duty to be configured for the motor specified 
-	ramge from 1 - 99
-    
-        Returns
-        -------
-        none    
         """
-        self.microblaze.write_mailbox(0, mot)
+        if motor not in [self.MOTOR_A, self.MOTOR_B]:
+            raise ValueError('Motor index can only be {} or {}.'.format(
+                self.MOTOR_A, self.MOTOR_B))
+        if speed not in range(1, 100):
+            raise ValueError('Motor speed must be in range [1, 99].')
+        self.microblaze.write_mailbox(0, motor)
         self.microblaze.write_mailbox(0x4, speed)
         self.microblaze.write_blocking_command(SET_SPEED)
+        if motor == self.MOTOR_A:
+            self.motor_a['speed'] = speed
+        else:
+            self.motor_b['speed'] = speed
 
-        return self.microblaze.read_mailbox(0)
+    def run(self, motor):
+        """Run the motor at the set speed and direction.
 
-    def run(self, mot):
-        """Run the motor at the set speed and direction
+        Motor A is the motor connected to pin pair (2,3) or (8,9).
+        Motor B is the motor connected to pin pair (4,11) or (7,10).
 
-	Parameters
+        Parameters
         ----------
-        mot : int
-	0 - Motor A (connected to (2,3) or (8,9))
-	1 - Motor B (connected to (4,11) or (7,10))
-    
-        Returns
-        -------
-        none    
+        motor : int
+            Can be MOTOR_A(0) or MOTOR_B(1).
+
         """
-        self.microblaze.write_mailbox(0, mot)
+        self.microblaze.write_mailbox(0, motor)
         self.microblaze.write_blocking_command(RUN)
+        if motor == self.MOTOR_A:
+            self.motor_a['status'] = 'running'
+        else:
+            self.motor_b['status'] = 'running'
 
-        return self.microblaze.read_mailbox(0)
+    def stop(self, motor):
+        """Stop the specified motor.
 
-    def stop(self, mot):
-        """Stop the specified motor
+        Motor A is the motor connected to pin pair (2,3) or (8,9).
+        Motor B is the motor connected to pin pair (4,11) or (7,10).
 
-	Parameters
+        Parameters
         ----------
-        mot : int
-	0 - Motor A (connected to (2,3) or (8,9))
-	1 - Motor B (connected to (4,11) or (7,10))
-    
-        Returns
-        -------
-        none    
+        motor : int
+            Can be MOTOR_A(0) or MOTOR_B(1).
+
         """
-        self.microblaze.write_mailbox(0, mot)
+        self.microblaze.write_mailbox(0, motor)
         self.microblaze.write_blocking_command(STOP)
-
-        return self.microblaze.read_mailbox(0)
-
-    
+        if motor == self.MOTOR_A:
+            self.motor_a['status'] = 'stopped'
+        else:
+            self.motor_b['status'] = 'stopped'
