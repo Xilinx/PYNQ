348559f90240b616a077c5e64f05faaae1c8fc4e
Rock Qu <yunq@xilinx.com>
2018-06-14 09:12:48 -0700

    copy overlays into pynq build

--- sdbuild/packages/pynq/pre.sh
+++ sdbuild/packages/pynq/pre.sh
@@ -7,6 +7,10 @@ target=$1
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo cp -r $BUILD_ROOT/PYNQ $target/home/xilinx/pynq_git
+if [ ${PYNQ_BOARD} != "Unknown" ]; then
+	sudo cp -rf ${PYNQ_BOARDDIR} \
+	$target/home/xilinx/pynq_git/boards/
+fi
 sudo cp $script_dir/pl_server.sh $target/usr/local/bin
 sudo cp $script_dir/pl_server.service $target/lib/systemd/system
 sudo cp $script_dir/pynq_hostname.sh $target/usr/local/bin
