7da49a72328152609031b628be40b4403d1373bd
Peter Ogden <ogden@xilinx.com>
2018-06-25 14:45:52 +0000

    Make ipythonmicroblaze work on 64-bit architectures

--- pynq/lib/pynqmicroblaze/rpc.py
+++ pynq/lib/pynqmicroblaze/rpc.py
@@ -39,6 +39,7 @@ from pycparser import c_ast
 from pycparser import c_generator
 from pycparser.plyparser import ParseError
 from copy import deepcopy
+from pynq.ps import ZU_ARCH, CPU_ARCH, ZYNQ_ARCH
 
 from .compile import preprocess
 from .streams import SimpleMBStream
@@ -49,6 +50,12 @@ from . import MicroblazeProgram
 _parser = pycparser.CParser()
 _generator = c_generator.CGenerator()
 
+if CPU_ARCH == ZYNQ_ARCH:
+    PTR_OFFSET = "0x20000000"
+elif CPU_ARCH == ZU_ARCH:
+    PTR_OFFSET = "0x80000000"
+else:
+    PTR_OFFSET = "0x0"
 
 # First we define a series of classes to represent types
 # Each class is responsible for one particular type of C
@@ -114,7 +121,7 @@ class VoidPointerWrapper:
         commands.append(c_ast.Assignment(
             '|=',
             c_ast.ID(name + '_int'),
-            c_ast.Constant('int', '0x20000000')))
+            c_ast.Constant('int', PTR_OFFSET)))
         commands.append(
             c_ast.Decl(name, [], [], [],
                        c_ast.PtrDecl(
@@ -269,9 +276,9 @@ def _type_to_struct_string(tdecl):
                 return 'Q'
         else:
             if signed:
-                return 'l'
+                return 'i'
             else:
-                return 'L'
+                return 'I'
     if name == 'short':
         if signed:
             return 'h'
@@ -649,7 +656,7 @@ def _function_wrapper(stream, index, adapter, return_type, *args):
     with functools.partial to build a new thing
 
     """
-    arg_string = struct.pack('l', index)
+    arg_string = struct.pack('i', index)
     arg_string += adapter.pack_args(*args)
     stream.write(arg_string)
     if not adapter.returns:
@@ -720,7 +727,7 @@ class MicroblazeFunction:
         self.return_type = return_type
 
     def _call_function(self, *args):
-        arg_string = struct.pack('l', self.index)
+        arg_string = struct.pack('i', self.index)
         arg_string += self.function.pack_args(*args)
         self.stream.write(arg_string)
 
