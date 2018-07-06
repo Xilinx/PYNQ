6ec6ef6bd83e4285245c2a91928fe75bf17d69b7
David Brochart <david.brochart@gmail.com>
2018-07-06 18:34:19 +0200

    Replace self with Xlnk

--- pynq/xlnk.py
+++ pynq/xlnk.py
@@ -368,7 +368,7 @@ class Xlnk:
         None
 
         """
-        self.ffi.memmove(dest, src, nbytes)
+        Xlnk.ffi.memmove(dest, src, nbytes)
     
     @staticmethod
     def cma_cast(data, data_type="void"):
@@ -391,7 +391,7 @@ class Xlnk:
             Pointer to buffer with specified data type.
             
         """
-        return self.ffi.cast(data_type+"*", data)
+        return Xlnk.ffi.cast(data_type+"*", data)
       
     def cma_free(self, buf):
         """Free a previously allocated buffer.
