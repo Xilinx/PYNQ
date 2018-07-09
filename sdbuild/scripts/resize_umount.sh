4ce1407d4b854458f6a8a49de9bd85f17aad6fa0
Peter Ogden <ogden@xilinx.com>
2018-07-09 04:31:38 -0400

    Fix resize script

--- sdbuild/scripts/resize_umount.sh
+++ sdbuild/scripts/resize_umount.sh
@@ -37,7 +37,7 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk $1
   p # primary partition
   2 # partion number 2
     # default, start immediately after preceding partition
-  +$(( $new_size * 2 ))  # New size in 512 byte blocks
+  +$(( $new_size * 2 - 1 ))  # New size in 512 byte blocks
   p # print the in-memory partition table
   w # write the partition table
   q # and we're done
