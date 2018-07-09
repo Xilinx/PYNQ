4174d05cc885a3169089b3cde5edd2dc854e042c
Peter Ogden <ogden@xilinx.com>
2018-07-09 06:37:13 -0700

    Install pandas separately on arm as DEBs are broken

---
+++ sdbuild/packages/pandas/qemu.sh
@@ -0,0 +1,26 @@
+export PATH=/opt/python3.6/bin:$PATH
+export HOME=/root
+
+set -x
+
+iter_count=0
+max_iterations=3
+
+read -d '' PACKAGES <<EOT
+pandas
+EOT
+
+while [ -n "$PACKAGES" -a "$max_iterations" != "$iter_count" ];
+do 
+  printf '%s\n' "$PACKAGES" | while IFS= read -r p
+  do 
+    python3.6 -m pip install -v $p
+    result=$?
+    if [ $result != "0" ]; then
+      echo "Package $p installed failed" >> pip.failed
+      failed_packages="$failed_packages $p"
+    fi
+  done
+  iter_count=$(( $iter_count + 1 ))
+  PACKAGES="$failed_packages"
+done
