b2b537757a2fcd6878c079bf780c5a7e33dc2a59
Peter Ogden <ogden@xilinx.com>
2018-07-11 10:37:14 -0400

    Add X11 setup for ZU+

---
+++ sdbuild/packages/x11/qemu.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+set -x
+set -e
+
+if [ -f /usr/local/share/x11/Welcome\ to\ Pynq.ipynb ]; then
+	jupyter nbconvert --to html \
+	/usr/local/share/x11/Welcome\ to\ Pynq.ipynb
+	rm -f /usr/local/share/x11/Welcome\ to\ Pynq.ipynb
+fi
+
+systemctl enable pynq-x11.service
+
+chown -R xilinx:xilinx /home/xilinx/.config/chromium
+chmod a+x /usr/bin/killchromium
+
+mkdir /root/armsoc_build
+cd /root/armsoc_build
+
+git clone https://anongit.freedesktop.org/git/xorg/driver/xf86-video-armsoc.git
+cd xf86-video-armsoc
+git apply /armsoc.patch --ignore-whitespace
+./autogen.sh
+./configure --prefix=/usr
+make -j4
+make install
+cd /
+rm -rf /root/armsoc_build
+rm /armsoc.patch
