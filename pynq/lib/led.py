01c4c602bc864e518bb3f633504be59f87df66af
kevja <40243625+kevja@users.noreply.github.com>
2018-08-21 15:01:16 -0600

    update with isinstance; add __len__

--- pynq/lib/led.py
+++ pynq/lib/led.py
@@ -2,13 +2,15 @@ __author__ = "Kevin Anderson"
 __copyright__ = "Copyright 2018, Xilinx"
 __email__ = "pynq_support@xilinx.com"
 
+from pynq.lib.gpio_abc import GPIO_Output
+
 class LED(object):
 	def __init__(self, device, length=1):
-		methods = ['on', 'off', 'toggle']
-		if(all(m in dir(device) for m in methods)):
+		if(isinstance(device, GPIO_Output)):
 			self._impl = device
+			self._length = length
 		else:
-		 	raise TypeError("Object must contain LED methods: %s", dir(self))  # <<<  Should it raise an Exception????
+		 	raise TypeError("Object must contain methods: " + str(GPIO_Output.methods))
 
 	def on(self):
 		self._impl.on()
@@ -20,4 +22,4 @@ class LED(object):
 		self._impl.toggle()
 
 	def __len__(self):
-		return 1;
+		return self._length;
