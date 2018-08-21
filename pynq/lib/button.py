8fe7b2094f4ea566ada9313778bd551390853493
kevja <40243625+kevja@users.noreply.github.com>
2018-08-21 15:02:04 -0600

    update with isinstance; add __len__

--- pynq/lib/button.py
+++ pynq/lib/button.py
@@ -2,18 +2,21 @@ __author__ = "Kevin Anderson"
 __copyright__ = "Copyright 2018, Xilinx"
 __email__ = "pynq_support@xilinx.com"
 
-import asyncio
+from pynq.lib.gpio_abc import GPIO_Input
 
 class Button(object):
 	def __init__(self, device, length=1):
-		methods = ['read', 'wait_for_value_async', 'wait_for_value']
-		if(all(m in dir(device) for m in methods)):
+		if(isinstance(device, GPIO_Input)):
 			self._impl = device
+			self._length = length
 		else:
-			raise TypeError("Object must contain LED methods: %s", methods)
+			raise TypeError("Object must contain methods: " + str(GPIO_Input.methods))
 	
 	def read(self):
 		return self._impl.read()
 
 	def wait_for_value(self, value):
 		self._impl.wait_for_value(value)
+
+	def __len__(self):
+		return self._length;
