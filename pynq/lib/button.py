f037a5de73e9ae6ce6a46dcb095c13002cbb1bbc
kevanderson <Kevin_Anderson@brown.edu>
2018-08-22 22:17:22 +0000

    update GPIO classes

--- pynq/lib/button.py
+++ pynq/lib/button.py
@@ -27,13 +27,7 @@
 #   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 #   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import asyncio
-from pynq import MMIO
-from pynq import PL
-from pynq import Interrupt
-
-
-__author__ = "Giuseppe Natale"
+__author__ = "Kevin Anderson"
 __copyright__ = "Copyright 2016, Xilinx"
 __email__ = "pynq_support@xilinx.com"
 
@@ -43,80 +37,41 @@ class Button(object):
 
     Attributes
     ----------
-    index : int
-        Index of the push-buttons, starting from 0.
+    _impl : object
+        An object with appropriate Button methods
 
     """
-    _mmio = None
 
-    def __init__(self, index):
+    def __init__(self, device):
         """Create a new Button object.
 
         Parameters
         ----------
-        index : int
-            The index of the push-buttons, from 0 to 3.
+        device : object
+            An object with appropriate Button methods:
+            read, wait_for_value
 
         """
-        if Button._mmio is None:
-            base_addr = PL.ip_dict["btns_gpio"]["phys_addr"]
-            Button._mmio = MMIO(base_addr, 512)
-        self.index = index
-        self.interrupt = None
-        try:
-            self.interrupt = Interrupt('btns_gpio/ip2intc_irpt')
-            # Enable interrupts
-            Button._mmio.write(0x11C, 0x80000000)
-            Button._mmio.write(0x128, 0x00000001)
-        except ValueError:
-            pass
+        methods = ['read', 'wait_for_value']
+        if all(m in dir(device) for m in methods):
+            self._impl = device
+        else:
+            raise TypeError("'device' must contain Button methods: " +
+                            str(methods))
 
     def read(self):
-        """Read the current value of the button.
-
-        Returns
-        -------
-        int
-            Either 1 if the button is pressed or 0 otherwise
-
-        """
-        curr_val = Button._mmio.read()
-        return (curr_val & (1 << self.index)) >> self.index
-
-    @asyncio.coroutine
-    def wait_for_value_async(self, value):
-        """Wait for the button to be pressed or released
-
-        Parameters
-        ----------
-        value: int
-            1 to wait for press or 0 to wait for release
-
-        This function is an asyncio coroutine
-
-        """
-        if self.interrupt is None:
-            raise RuntimeError('Interrupts not available in this Overlay')
-        while self.read() != value:
-            yield from self.interrupt.wait()
-            if Button._mmio.read(0x120) & 0x1:
-                Button._mmio.write(0x120, 0x00000001)
+        """Read the current value of the button."""
+        return self._impl.read()
 
     def wait_for_value(self, value):
-        """Wait for the button to be pressed or released
+        """Wait for the button to be pressed or released.
 
         Parameters
         ----------
         value: int
             1 to wait for press or 0 to wait for release
 
-        This function wraps the coroutine form so the asyncio
-        event loop will run until the function returns
-
         """
-        if self.interrupt is None:
-            raise RuntimeError('Interrupts not available in this Overlay')
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(asyncio.ensure_future(
-            self.wait_for_value_async(value)
-        ))
+        if (value != 1) or (value != 0):
+            raise ValueError("'value' must be 0 or 1.")
+        self._impl.wait_for_value(value)
