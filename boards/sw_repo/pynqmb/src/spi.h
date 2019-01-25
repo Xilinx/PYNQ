5b93c3093158ce0a830f4fea43a6a4f154f59826
Jack Rosenthal <jack@rosenth.al>
2019-01-25 13:41:06 -0700

    pynqmb: SPI transfer supports read w/o write (and visa versa) (#679)
    * pynqmb: SPI transfer supports read w/o write (and visa versa)
    
    * pynqmb: Initial SPI steps are not required when only preforming read operation
    
    * pynqmb: Defined behavior for read without write on SPI

--- boards/sw_repo/pynqmb/src/spi.h
+++ boards/sw_repo/pynqmb/src/spi.h
@@ -62,14 +62,14 @@
 typedef int spi;
 
 spi spi_open_device(unsigned int device);
-spi spi_open(unsigned int spiclk, unsigned int miso, 
+spi spi_open(unsigned int spiclk, unsigned int miso,
              unsigned int mosi, unsigned int ss);
-spi spi_configure(spi dev_id, unsigned int clk_phase, 
-                   unsigned int clk_polarity);
-void spi_transfer(spi dev_id, const char* write_data, char* read_data, 
+spi spi_configure(spi dev_id, unsigned int clk_phase,
+                  unsigned int clk_polarity);
+void spi_transfer(spi dev_id, const char *write_data, char *read_data,
                   unsigned int length);
 void spi_close(spi dev_id);
 unsigned int spi_get_num_devices(void);
 
 #endif
-#endif  // _SPI_H_
+#endif                          // _SPI_H_
