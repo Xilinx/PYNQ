a932c31ca9d46d4ca33404bddb7296fb03087135
Chenglong Hu <sonnyhcl@163.com>
2019-01-26 06:34:58 +0800

    modify xczu7ev-ffvc1156-2-i-es2 to xczu7ev-ffvc1156-2-i (#775)

--- boards/ip/hls/pixel_unpack_2/script.tcl
+++ boards/ip/hls/pixel_unpack_2/script.tcl
@@ -3,7 +3,7 @@ set_top pixel_unpack_2
 add_files pixel_unpack_2/pixel_unpack.cpp
 add_files -tb pixel_unpack_2/pixel_unpack_test.cpp
 open_solution "solution1"
-set_part {xczu7ev-ffvc1156-2-i-es2}
+set_part {xczu7ev-ffvc1156-2-i}
 create_clock -period 3.3 -name default
 csynth_design
 export_design -format ip_catalog -description "Pixel Unpacking from 32-bit to 24-bit" -display_name "Pixel Unpack (2ppc)"
