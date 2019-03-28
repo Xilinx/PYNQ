37cf3b3c3da7507c0dd39641903ebfa19d5c9838
Rock Qu <yunq@xilinx.com>
2019-03-27 17:03:24 -0700

    add git URL to REVISION

--- sdbuild/packages/pynq/get_revision.sh
+++ sdbuild/packages/pynq/get_revision.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "Release $(date +'%Y_%m_%d') $(git rev-parse --short=7 --verify HEAD)"
+echo "Release $(date +'%Y_%m_%d')"\
+	"$(git rev-parse --short=7 --verify HEAD)"\
+	"$(git config --get remote.origin.url)"
 cd /home/xilinx/pynq_git/boards
 if [ -d .git ]; then
-	echo "Board $(date +'%Y_%m_%d') $(git rev-parse --short=7 --verify HEAD)"
+	echo "Board $(date +'%Y_%m_%d')"\
+		"$(git rev-parse --short=7 --verify HEAD)"\
+		"$(git config --get remote.origin.url)"
 fi
