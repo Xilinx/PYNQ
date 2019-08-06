286279518bb3e8f56a4bffe2c35361dc3fd9ea7e
Yun Rock Qu <yunq@xilinx.com>
2019-08-06 12:27:40 -0700

    remove url for pynq because .git not working inside repo (#860)

--- sdbuild/packages/pynq/get_revision.sh
+++ sdbuild/packages/pynq/get_revision.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo "Release $(date +'%Y_%m_%d')"\
-	"$(git rev-parse --short=7 --verify HEAD)"\
-	"$(git config --get remote.origin.url)"
+	"$(git rev-parse --short=7 --verify HEAD)"
 cd /home/xilinx/pynq_git/boards
 if [ -d .git ]; then
 	echo "Board $(date +'%Y_%m_%d')"\
