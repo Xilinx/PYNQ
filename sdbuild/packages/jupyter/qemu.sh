8bc7cee9d0cfb5f05fa160b59913d46b602373a1
Yun Rock Qu <yunq@xilinx.com>
2019-08-06 12:27:29 -0700

    fix python paths (#859)

--- sdbuild/packages/jupyter/qemu.sh
+++ sdbuild/packages/jupyter/qemu.sh
@@ -3,16 +3,15 @@
 set -x
 set -e
 
-export PATH=/opt/python3.6/bin:$PATH
 export HOME=/root
 export PYNQ_PYTHON=python3.6
+export PYNQ_JUPYTER_NOTEBOOKS=/home/xilinx/jupyter_notebooks
 
-export HOME=/root
 jupyter notebook --generate-config --allow-root
 
 cat - >> /root/.jupyter/jupyter_notebook_config.py <<EOT
 c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.notebook_dir = '/home/xilinx/jupyter_notebooks'
+c.NotebookApp.notebook_dir = '$PYNQ_JUPYTER_NOTEBOOKS'
 c.NotebookApp.password = 'sha1:46c5ef4fa52f:ee46dad5008c6270a52f6272828a51b16336b492'
 c.NotebookApp.port = 9090
 c.NotebookApp.iopub_data_rate_limit = 100000000
@@ -28,6 +27,6 @@ jupyter nbextension enable rise --py --sys-prefix
 # Enable jupyterlab
 jupyter serverextension enable jupyterlab
 
-mkdir -p /home/xilinx/jupyter_notebooks
+mkdir -p $PYNQ_JUPYTER_NOTEBOOKS
 
 systemctl enable jupyter
