7ff82e664dcfaa74099af806d4d0c18a3beb6c0b
Peter Ogden <ogden@xilinx.com>
2019-08-20 13:53:36 +0100

    Move bitstream download to XlnkDevice

--- pynq/pl_server/device.py
+++ pynq/pl_server/device.py
@@ -380,17 +380,15 @@ class Device(metaclass=DeviceMeta):
                     warnings.warn("Timeout for shutdown manager. It's likely "
                                   "the configured bitstream and metadata "
                                   "don't match.")
-    def post_download(self):
-        """Hook called after a new bitstream is loaded
 
-        """
-        pass
-
-    def pre_download(self):
-        """Hook called before a new bitstream is loaded
-
-        """
-        self.shutdown()
+    def post_download(self, bitstream, parser):
+        if not bitstream.partial:
+            import datetime
+            t = datetime.datetime.now()
+            bitstream.timestamp = "{}/{}/{} {}:{}:{} +{}".format(
+                    t.year, t.month, t.day,
+                    t.hour, t.minute, t.second, t.microsecond)
+            self.reset(parser, bitstream.timestamp, bitstream.bitfile_name)
 
     def has_capability(self, cap):
         """Test if the device as a desired capability
@@ -410,11 +408,99 @@ class Device(metaclass=DeviceMeta):
             return False
         return cap in self.capabilities and self.capabilities[cap]
 
+def parse_bit_header(self, bitstream):
+    """The method to parse the header of a bitstream.
+
+    The returned dictionary has the following keys:
+    "design": str, the Vivado project name that generated the bitstream;
+    "version": str, the Vivado tool version that generated the bitstream;
+    "part": str, the Xilinx part name that the bitstream targets;
+    "date": str, the date the bitstream was compiled on;
+    "time": str, the time the bitstream finished compilation;
+    "length": int, total length of the bitstream (in bytes);
+    "data": binary, binary data in .bit file format
+
+    Returns
+    -------
+    Dict
+        A dictionary containing the header information.
+
+    Note
+    ----
+    Implemented based on: https://blog.aeste.my/?p=2892
+
+    """
+    with open(bitfile, 'rb') as bitf:
+        finished = False
+        offset = 0
+        contents = bitf.read()
+        bit_dict = {}
+
+        # Strip the (2+n)-byte first field (2-bit length, n-bit data)
+        length = struct.unpack('>h', contents[offset:offset + 2])[0]
+        offset += 2 + length
+
+        # Strip a two-byte unknown field (usually 1)
+        offset += 2
+
+        # Strip the remaining headers. 0x65 signals the bit data field
+        while not finished:
+            desc = contents[offset]
+            offset += 1
+
+            if desc != 0x65:
+                length = struct.unpack('>h',
+                                       contents[offset:offset + 2])[0]
+                offset += 2
+                fmt = ">{}s".format(length)
+                data = struct.unpack(fmt,
+                                     contents[offset:offset + length])[0]
+                data = data.decode('ascii')[:-1]
+                offset += length
+
+            if desc == 0x61:
+                s = data.split(";")
+                bit_dict['design'] = s[0]
+                bit_dict['version'] = s[-1]
+            elif desc == 0x62:
+                bit_dict['part'] = data
+            elif desc == 0x63:
+                bit_dict['date'] = data
+            elif desc == 0x64:
+                bit_dict['time'] = data
+            elif desc == 0x65:
+                finished = True
+                length = struct.unpack('>i',
+                                       contents[offset:offset + 4])[0]
+                offset += 4
+                # Expected length values can be verified in the chip TRM
+                bit_dict['length'] = str(length)
+                if length + offset != len(contents):
+                    raise RuntimeError("Invalid length found")
+                bit_dict['data'] = contents[offset:offset + length]
+            else:
+                raise RuntimeError("Unknown field: {}".format(hex(desc)))
+        return bit_dict
+
+def _preload_binfile(self, bitstream):
+    bitstream.binfile_name = os.path.basename(
+        bitstream.bitfile_name).replace('.bit', '.bin')
+    bitstream.firmware_path = os.path.join('/lib/firmware',
+                                           bitstream.binfile_name)
+    bit_dict = parse_bit_header(bitstream.bitfile_name)
+    if bit_dict != bitstream.bit_data:
+        bitstream.bit_data = bit_dict
+        bit_buffer = np.frombuffer(bit_dict['data'], 'i4')
+        bin_buffer = bit_buffer.byteswap()
+        bin_buffer.tofile(bitstream.firmware_path, "")
 
 class XlnkDevice(Device):
     """Device sub-class for Xlnk based devices
 
     """
+    BS_FPGA_MAN = "/sys/class/fpga_manager/fpga0/firmware"
+    BS_FPGA_MAN_FLAGS = "/sys/class/fpga_manager/fpga0/flags"
+
     @classmethod
     def _probe_(cls):
         if os.path.exists('/dev/xlnk'):
@@ -459,3 +545,19 @@ class XlnkDevice(Device):
         os.close(mmap_file)
         array = np.frombuffer(mem, np.uint32, length >> 2, virt_offset)
         return array
+
+    def download(self, bitstream, parser=None):
+        if not bitstream.binfile_name:
+            _preload_binfile(bitstream)
+
+        if not bitstream.partial:
+            self.shutdown()
+            flag = '0'
+        else:
+            flag = '1'
+        with open(self.BS_FPGA_MAN_FLAGS, 'w') as fd:
+            fd.write(flag)
+        with open(self.BS_FPGA_MAN, 'w') as fd:
+            fd.write(bitstream.binfile_name)
+
+        super().post_download(bitstream, parser)
