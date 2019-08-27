f6717d19abb8964f88ffa84dd27b95c5ebf11088
Rock Qu <yunq@xilinx.com>
2019-08-27 10:35:23 -0700

    building microblaze bin when installing pynq

--- sdbuild/packages/pynq/pre.sh
+++ sdbuild/packages/pynq/pre.sh
@@ -24,12 +24,40 @@ build_bitstreams () {
 }
 
 # build all default bitstreams since they will be included in the sdist
-# this happens when we install pynq at stage 3 
+# this happens when we install pynq at stage 3
 default_boards="Pynq-Z1 Pynq-Z2 ZCU104"
 if [ ${PYNQ_BOARD} = "Unknown" ]; then
 	for b in $default_boards ; do
 		build_bitstreams "$BUILD_ROOT/PYNQ/boards" "$b"
 	done
+fi
+
+# build all the microblaze bsp's using Pynq-Z2's hdf
+# this happens when we install pynq at stage 3
+if [ ${PYNQ_BOARD} = "Unknown" ]; then
+	orig_dir=$(pwd)
+	cd $BUILD_ROOT/PYNQ/boards/sw_repo
+	make HDF=../Pynq-Z2/base/base.hdf HW_DEF=hw_base
+
+	cd bsp_iop_arduino_mb/iop_arduino_mb && rm -rf code libsrc && cd -
+	cp -rf bsp_iop_arduino_mb $BUILD_ROOT/PYNQ/pynq/lib/arduino/bsp_iop_arduino
+	cd $BUILD_ROOT/PYNQ/pynq/lib/arduino && make && cd $BUILD_ROOT/PYNQ/boards/sw_repo
+
+	cd bsp_iop_pmoda_mb/iop_pmoda_mb && rm -rf code libsrc && cd -
+	cp -rf bsp_iop_pmoda_mb $BUILD_ROOT/PYNQ/pynq/lib/pmod/bsp_iop_pmod
+	cd $BUILD_ROOT/PYNQ/pynq/lib/pmod && make && cd $BUILD_ROOT/PYNQ/boards/sw_repo
+
+	cd bsp_iop_rpi_mb/iop_rpi_mb && rm -rf code libsrc && cd -
+	cp -rf bsp_iop_rpi_mb $BUILD_ROOT/PYNQ/pynq/lib/rpi/bsp_iop_rpi
+
+	make clean && make HDF=../Pynq-Z2/logictools/logictools.hdf HW_DEF=hw_logictools
+
+	cd bsp_lcp_ar_mb/lcp_ar_mb && rm -rf code libsrc && cd -
+	cp -rf bsp_lcp_ar_mb $BUILD_ROOT/PYNQ/pynq/lib/logictools/bsp_lcp_ar_mb
+	cd $BUILD_ROOT/PYNQ/pynq/lib/logictools && make && cd $BUILD_ROOT/PYNQ/boards/sw_repo
+
+	make clean
+	cd $orig_dir
 fi
 
 # build missing bitstream, only if any board/overlay can be located
@@ -39,7 +67,7 @@ if [ ${PYNQ_BOARD} != "Unknown" ]; then
 fi
 
 # copy files, excluding sdist, onto qemu image
-# copy default board files at stage 3; copy additional  board files at stage 4
+# copy default board files at stage 3; copy additional board files at stage 4
 if [ ${PYNQ_BOARD} == "Unknown" ]; then
 	board_dir="$BUILD_ROOT/PYNQ/boards/Pynq-Z1 \
 		   $BUILD_ROOT/PYNQ/boards/Pynq-Z2 \
@@ -87,3 +115,4 @@ sudo cp $script_dir/pl_server.sh $target/usr/local/bin
 sudo cp $script_dir/pl_server.service $target/lib/systemd/system
 sudo cp $script_dir/pynq_hostname.sh $target/usr/local/bin
 sudo cp $script_dir/boardname.sh $target/etc/profile.d
+
