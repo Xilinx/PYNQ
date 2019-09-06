a155f825cfd45c68f6c96dd54db1ffdc07b61937
Yun Rock Qu <yunq@xilinx.com>
2019-09-06 16:15:41 -0700

    build pynq from sdist and add sdist target (#906)
    * build pynq from sdist and add sdist target
    
    * building microblaze bin when installing pynq
    
    * refactor build.sh from pre.sh
    
    * skip the entire bsp building process to reduce build time

---
+++ sdbuild/packages/pynq/post.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+set -e
+
+target=$1
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# sdist is built at stage 3
+if [ ${PYNQ_BOARD} == "Unknown" ]; then
+	dist_out=$BUILD_ROOT/PYNQ/dist
+	mkdir -p $dist_out
+	cp -rf $target/home/xilinx/pynq_git/dist/* $dist_out
+fi
+sudo rm -rf $target/home/xilinx/pynq_git $target/home/xilinx/get_revision.sh
