4466458528995dfebcd413485483e78c3c41e68a
Peter Ogden <ogden@xilinx.com>
2019-09-12 21:44:57 +0100

    Update Microblaze GCC to work with 2019.1 tools (#925)
    * fix https://github.com/Xilinx/PYNQ-Dev/issues/195
    
    * Build Microblaze compiler from Ubuntu sysroot to avoid libc version issues
    
    * Add Microblaze patches from 2019.1

--- sdbuild/packages/gcc-mb/build.sh
+++ sdbuild/packages/gcc-mb/build.sh
@@ -5,6 +5,7 @@ set -e
 set -x
 
 ARCH=$1
+ROOT_IMAGE=$2
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 case ${ARCH} in
@@ -20,6 +21,23 @@ case ${ARCH} in
     ;;
 
 esac
+
+SYSROOT_IMAGE=$PWD/sysroot.${ARCH}.img
+export HOST_SYSROOT=$PWD/sysroot.${ARCH}
+mkdir -p ${HOST_SYSROOT}
+cp --sparse=always $ROOT_IMAGE $SYSROOT_IMAGE
+${ROOTDIR}/scripts/mount_image.sh $SYSROOT_IMAGE $HOST_SYSROOT
+
+function unmount_delete() {
+${ROOTDIR}/scripts/unmount_image.sh $HOST_SYSROOT $SYSROOT_IMAGE
+rm $SYSROOT_IMAGE
+}
+
+trap unmount_delete EXIT
+
+# Fixup symlinks in the sysroot
+(cd $HOST_SYSROOT && for link in `find -lname '/*'`; do target=$(readlink "$link"); sudo rm "$link"; sudo ln -s "$PWD$target" "$link"; done)
+
 export -n LD_LIBRARY_PATH
 # Use cross tools to build the provided configuration
 ct-ng $sample
