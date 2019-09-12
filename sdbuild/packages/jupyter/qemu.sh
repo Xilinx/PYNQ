be892fa8c78869b7d009e5f7bfcf9b6e7c5ea270
Peter Ogden <ogden@xilinx.com>
2019-09-12 21:43:41 +0100

    Add Jupyterlab extension for rfsoc demo (#920)
    * Add Jupyterlab extension for rfsoc demo
    
    * Reduce the memory usage for compiling labextensions on arm
    
    * Fix sed script
    
    * Change sed target

--- sdbuild/packages/jupyter/qemu.sh
+++ sdbuild/packages/jupyter/qemu.sh
@@ -30,6 +30,17 @@ jupyter nbextension enable rise --py --sys-prefix
 # Enable jupyterlab
 jupyter serverextension enable jupyterlab
 
+jupyter labextension install @jupyter-widgets/jupyterlab-manager@1.0.2 --no-build
+jupyter labextension install plotlywidget@1.1.0 --no-build
+jupyter labextension install jupyterlab-plotly@1.1.0 --no-build
+
+if [ ${ARCH} == 'arm' ]; then
+  sed 's:4096:2048:g' -i /usr/local/lib/python3.6/dist-packages/jupyterlab/staging/package.json
+fi
+
+jupyter lab build
+rm -rf /usr/local/share/jupyter/lab/staging
+
 mkdir -p $PYNQ_JUPYTER_NOTEBOOKS
 
 systemctl enable jupyter
