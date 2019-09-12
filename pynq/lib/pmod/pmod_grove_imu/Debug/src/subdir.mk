1689fa64f17d5eb37633b6278b8e7dbde11ce2fd
Yun Rock Qu <yunq@xilinx.com>
2019-09-12 13:43:08 -0700

    remove bsp and bin from pmod (#919)
    * remove bsp and bin from pmod
    
    * remove build targets from sdist

--- pynq/lib/pmod/pmod_grove_imu/Debug/src/subdir.mk
+++ pynq/lib/pmod/pmod_grove_imu/Debug/src/subdir.mk
@@ -20,7 +20,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MicroBlaze gcc compiler'
-	mb-gcc -Wall -O1 -g3 -c -fmessage-length=0 -MT"$@" -I../../bsp_iop_pmod/iop_pmoda_mb/include -mlittle-endian -mcpu=v10.0 -mxl-soft-mul -Wl,--no-relax -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	mb-gcc -Wall -O1 -g3 -c -fmessage-length=0 -MT"$@" -I../../bsp_iop_pmod/iop_pmoda_mb/include -mlittle-endian -mcpu=v11.0 -mxl-soft-mul -Wl,--no-relax -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
