df12429a87e4fc10382db5439677713212f70b7f
Peter Ogden <ogden@xilinx.com>
2020-02-13 04:09:37 -0500

    Add ReprDict docstrings

--- pynq/utils.py
+++ pynq/utils.py
@@ -607,7 +607,24 @@ def _default_repr(obj):
     return repr(obj)
 
 class ReprDict(dict):
+    """Subclass of the built-in dict that will display using the Jupyterlab
+    JSON repr.
+
+    The class is recursive in that any entries that are also dictionaries
+    will be converted to ReprDict objects when returned.
+
+    """
     def __init__(self, *args, rootname="root", expanded=False, **kwargs):
+        """Dictionary constructor
+
+        Parameters
+        ----------
+        rootname : str
+            The value to display at the root of the tree
+        expanded : bool
+            Whether the view of the tree should start expanded
+
+        """
         self._rootname = rootname
         self._expanded = expanded
 
