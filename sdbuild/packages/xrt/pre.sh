bf563de120e8f54b15be319911aa5a1caadb02d5
Rock Qu <yunq@xilinx.com>
2020-05-19 18:51:29 -0700

    polishing xrt building flow

--- sdbuild/packages/xrt/pre.sh
+++ sdbuild/packages/xrt/pre.sh
@@ -6,8 +6,6 @@ set -e
 target=$1
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo cp $script_dir/*.patch $target/root
-
 cd $BUILD_ROOT/${PYNQ_BOARD}/petalinux_project
 petalinux-build -c zocl
 sudo cp -rf $BUILD_ROOT/${PYNQ_BOARD}/petalinux_project/build/tmp/sysroots-components/*/zocl/usr $target
