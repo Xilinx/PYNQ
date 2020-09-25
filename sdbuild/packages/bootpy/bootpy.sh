971209170aeaa1df3910cc916282e9adb233ede3
schelleg <graham.schelle@gmail.com>
2020-09-25 14:23:14 -0600

    bootpy cleanup to support more use cases

--- sdbuild/packages/bootpy/bootpy.sh
+++ sdbuild/packages/bootpy/bootpy.sh
@@ -2,23 +2,15 @@
 
 . /etc/environment
 
-if [ -z "$PYNQ_PYTHON" ]; then
-	PYNQ_PYTHON=python3.4
-else
-	PATH=/opt/$PYNQ_PYTHON/bin:$PATH
-fi
-
-
 BOOT_MNT=/boot
 BOOT_DEV=/dev/mmcblk0p1
+BOOT_PY=$BOOT_MNT/boot.py
 
-if [[ ! -d "$BOOT_MNT" ]]
-then
-    mkdir $BOOT_MNT
+if !(mount | grep -q "$BOOT_MNT") ; then
+   mount $BOOT_DEV $BOOT_MNT
 fi
 
-mount $BOOT_DEV $BOOT_MNT
-$PYNQ_PYTHON $BOOT_MNT/boot.py
-umount $BOOT_MNT
-
+if test -f "$BOOT_PY"; then
+   $PYNQ_PYTHON $BOOT_PY
+fi
 
