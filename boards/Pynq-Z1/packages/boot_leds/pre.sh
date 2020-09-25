53a1c5ee6ef77dffef0ce2579fef079ccbed39a0
Rock Qu <yunq@xilinx.com>
2020-09-25 10:16:59 -0700

    fix permission issue

--- boards/Pynq-Z1/packages/boot_leds/pre.sh
+++ boards/Pynq-Z1/packages/boot_leds/pre.sh
@@ -3,5 +3,5 @@
 target=$1
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo cat $script_dir/boot.py >> $target/boot/boot.py
+cat $script_dir/boot.py | sudo tee -a $target/boot/boot.py
 
