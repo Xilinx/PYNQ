Update cb_init function, remove floating operations from the microblaze. Fix the computation of the log_capacity

pynq/lib/arduino/arduino_analog.py
@@ -28,45 +28,24 @@
 #   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-import struct
-from math import ceil
+import numpy as np
 from . import Arduino
 from . import MAILBOX_OFFSET
 from . import ARDUINO_NUM_ANALOG_PINS
 
 
 __author__ = "Yun Rock Qu"
-__copyright__ = "Copyright 2016, Xilinx"
+__copyright__ = "Copyright 2016-2020, Xilinx"
 __email__ = "pynq_support@xilinx.com"
 
 
 ARDUINO_ANALOG_PROGRAM = "arduino_analog.bin"
 ARDUINO_ANALOG_LOG_START = MAILBOX_OFFSET+16
-ARDUINO_ANALOG_SAMPLES = 1000
+ARDUINO_MAX_SAMPLES = 1018
 CONFIG_IOP_SWITCH = 0x1
 GET_RAW_DATA = 0x3
-GET_VOLTAGE = 0x5
 READ_AND_LOG_RAW = 0x7
-READ_AND_LOG_FLOAT = 0x9
 RESET_ANALOG = 0xB
-
-
-def _reg2float(reg):
-    """Converts 32-bit register value to floats in Python.
-
-    Parameters
-    ----------
-    reg: int
-        A 32-bit register value read from the mailbox.
-
-    Returns
-    -------
-    float
-        A float number translated from the register value.
-
-    """
-    s = struct.pack('>l', reg)
-    return struct.unpack('>f', s)[0]
 
 
 class Arduino_Analog(object):
@@ -115,6 +94,11 @@ class Arduino_Analog(object):
         self.log_running = 0
         self.gr_pin = gr_pin
         self.num_channels = len(gr_pin)
+        # Calculate the offset address of the end of the log
+        self._samples_channel = ARDUINO_MAX_SAMPLES // self.num_channels
+        self._log_end = ARDUINO_ANALOG_LOG_START + 4 * self.num_channels * \
+                        self._samples_channel
+
 
         # Enable all the analog pins
         data = [0 for _ in range(ARDUINO_NUM_ANALOG_PINS)]
@@ -123,40 +107,36 @@ class Arduino_Analog(object):
         # Write configuration and wait for ACK
         self.microblaze.write_blocking_command(CONFIG_IOP_SWITCH)
 
-    def read_raw(self):
-        """Read the analog raw value from the analog peripheral.
+    def read(self,  rep = 'voltage'):
+        """Read the shared mailbox memory with the adc raw value
+         from the analog peripheral.
+
+        Parameters
+        ----------
+        rep : str
+            Selects the return type, either 'raw' or 'voltage'
         
         Returns
         -------
         list
-            The raw values from the analog device.
+            Either the raw values or the voltage depending on rep
         
         """
+        if rep not in ['raw', 'voltage']:
+            raise ValueError("rep can only be 'raw' or 'voltage'")
+            
         data_channels = 0
         for channel in self.gr_pin:
             data_channels |= (0x1 << channel)
         cmd = (data_channels << 8) + GET_RAW_DATA
         self.microblaze.write_blocking_command(cmd)
 
-        return self.microblaze.read_mailbox(0, self.num_channels)
+        reading = np.asarray(self.microblaze.read_mailbox(0, self.num_channels))
         
-    def read(self):
-        """Read the voltage value from the analog peripheral.
-        
-        Returns
-        -------
-        list
-            The float values after translation.
-        
-        """
-        data_channels = 0
-        for channel in self.gr_pin:
-            data_channels |= (0x1 << channel)
-        cmd = (data_channels << 8) + GET_VOLTAGE
-        self.microblaze.write_blocking_command(cmd)
-
-        raw = self.microblaze.read_mailbox(0, self.num_channels)
-        return [float("{0:.4f}".format(_reg2float(i))) for i in raw]
+        if rep == 'raw':
+            return reading
+        else:
+            return reading * V_Conv
         
     def set_log_interval_ms(self, log_interval_ms):
         """Set the length of the log for the analog peripheral.
@@ -182,8 +162,8 @@ class Arduino_Analog(object):
         self.log_interval_ms = log_interval_ms
         self.microblaze.write_mailbox(4, log_interval_ms)
 
-    def start_log_raw(self):
-        """Start recording raw data in a log.
+    def start_log(self):
+        """Start recording multiple analog samples (raw) values in a log.
         
         This method will first call set_log_interval_ms() before writing to
         the MMIO.
@@ -201,28 +181,8 @@ class Arduino_Analog(object):
             data_channels |= (0x1 << channel)
         cmd = (data_channels << 8) + READ_AND_LOG_RAW
         self.microblaze.write_non_blocking_command(cmd)
-                        
-    def start_log(self):
-        """Start recording multiple voltage values (float) in a log.
-        
-        This method will first call set_log_interval_ms() before writing to
-        the MMIO.
-            
-        Returns
-        -------
-        None
-        
-        """
-        self.log_running = 1
-        self.set_log_interval_ms(self.log_interval_ms)
-        
-        data_channels = 0
-        for channel in self.gr_pin:
-            data_channels |= (0x1 << channel)
-        cmd = (data_channels << 8) + READ_AND_LOG_FLOAT
-        self.microblaze.write_non_blocking_command(cmd)
 
-    def stop_log_raw(self):
+    def stop_log(self):
         """Stop recording the raw values in the log.
         
         Simply write 0xC to the MMIO to stop the log.
@@ -238,43 +198,32 @@ class Arduino_Analog(object):
         else:
             raise RuntimeError("No analog log running.")
             
-    def stop_log(self):
-        """Stop recording the voltage values in the log.
-        
-        This can be done by calling the stop_log_raw() method.
-            
-        Returns
-        -------
-        None
-        
-        """
-        if self.log_running == 1:
-            self.microblaze.write_non_blocking_command(RESET_ANALOG)
-            self.log_running = 0
-        else:
-            raise RuntimeError("No analog log running.")
-        
-    def get_log_raw(self):
+    def get_log(self, rep = 'voltage'):
         """Return list of logged raw samples.
-            
+
+        Parameters
+        ----------
+        rep : str
+            Selects the return type, either 'raw' or 'voltage'
+
         Returns
         -------
         list
-            List of valid raw samples from the analog device.
+            List of valid samples from the analog device, 
+            either 'raw' or 'voltage'
         
         """
         # Stop logging
         self.stop_log()
+
+        if rep not in ['raw', 'voltage']:
+            raise ValueError("rep can only be 'raw' or 'voltage'")
 
         # Prep iterators and results list
         [head_ptr, tail_ptr] = self.microblaze.read_mailbox(0x8, 2)
         readings = []
         for _ in range(self.num_channels):
             readings.append([])
-
-        # Calculate the log ending
-        log_end = ARDUINO_ANALOG_LOG_START + \
-            4*ARDUINO_ANALOG_SAMPLES*self.num_channels
 
         # Sweep circular buffer for samples
         if head_ptr == tail_ptr:
@@ -285,7 +234,7 @@ class Arduino_Analog(object):
                 for j in range(self.num_channels):
                     readings[j].append(raw[j])
         else:
-            for i in range(head_ptr, log_end, 4*self.num_channels):
+            for i in range(head_ptr, self._log_end, 4*self.num_channels):
                 raw = self.microblaze.read(i, self.num_channels)
                 for j in range(self.num_channels):
                     readings[j].append(raw[j])
@@ -295,54 +244,14 @@ class Arduino_Analog(object):
                 raw = self.microblaze.read(i, self.num_channels)
                 for j in range(self.num_channels):
                     readings[j].append(raw[j])
-        return readings
-
-    def get_log(self):
-        """Return list of logged samples.
-            
-        Returns
-        -------
-        list
-            List of valid voltage samples (floats) from the ADC sensor.
         
-        """
-        # Stop logging
-        self.stop_log()
-
-        # Prep iterators and results list
-        [head_ptr, tail_ptr] = self.microblaze.read_mailbox(0x8, 2)
-        readings = []
-        for _ in range(self.num_channels):
-            readings.append([])
+        readings_arr = np.asarray(readings)
         
-        # Calculate the log ending
-        log_end = ARDUINO_ANALOG_LOG_START + \
-            4*ARDUINO_ANALOG_SAMPLES*self.num_channels
-        
-        # Sweep circular buffer for samples
-        if head_ptr == tail_ptr:
-            return None
-        elif head_ptr < tail_ptr:
-            for i in range(head_ptr, tail_ptr, 4*self.num_channels):
-                raw = self.microblaze.read(i, self.num_channels)
-                for j in range(self.num_channels):
-                    readings[j].append(float("{0:.4f}".format(
-                        _reg2float(raw[j]))))
-
+        if rep == 'raw':
+            return readings_arr
         else:
-            for i in range(head_ptr, log_end, 4*self.num_channels):
-                raw = self.microblaze.read(i, self.num_channels)
-                for j in range(self.num_channels):
-                    readings[j].append(float("{0:.4f}".format(
-                        _reg2float(raw[j]))))
+            return readings_arr * V_Conv
 
-            for i in range(ARDUINO_ANALOG_LOG_START, tail_ptr,
-                           4*self.num_channels):
-                raw = self.microblaze.read(i, self.num_channels)
-                for j in range(self.num_channels):
-                    readings[j].append(float("{0:.4f}".format(
-                        _reg2float(raw[j]))))
-        return readings
         
     def reset(self):
         """Resets the system monitor for analog devices.