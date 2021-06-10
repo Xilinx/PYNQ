175f98e74539521ee7a13f68bb53d86efa71970b
lewis brown <lewis.brown.101@strath.ac.uk>
2021-06-10 16:15:39 +0100

    Reverting previous change

--- sdbuild/packages/xrfclk/boot.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env python3
-
-import xrfclk
-
-xrfclk.set_all_ref_clks(409.6)
-
--- sdbuild/packages/xrfclk/pre.sh
+++ sdbuild/packages/xrfclk/pre.sh
@@ -9,7 +9,3 @@ target=$1
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo cp -r $script_dir/package $target/root/xrfclk_build
-
-if [${BOARD} == "ZCU111"]; then
-cat $script_dir/boot.py | sudo tee -a $target/boot/boot.py
-fi
