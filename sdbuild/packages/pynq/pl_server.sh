e7abffd440a2833947be97f00c5f24c6ae55676c
Graham Schelle <graham.schelle@gmail.com>
2021-07-19 16:13:53 -0700

    PYNQ Revision into boot and python3.8 updates

--- sdbuild/packages/pynq/pl_server.sh
+++ sdbuild/packages/pynq/pl_server.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 . /etc/environment
+. /usr/local/share/pynq-venv/bin/activate
 
 if [ -z "$PYNQ_PYTHON" ]; then
-  PYNQ_PYTHON=python3.6
+  PYNQ_PYTHON=python3
 fi
 
 start_pl_server.py & 
