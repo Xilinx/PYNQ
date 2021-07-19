a7ed73a293f8a0bac52846fadd6017499852014a
Graham Schelle <graham.schelle@gmail.com>
2021-07-19 16:40:14 -0700

    pre-built folder to speed up armv7 rebuilds of python packages

---
+++ sdbuild/packages/python_packages_focal/post.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+set -e
+
+target=$1
+target_dir=root/.cache/pip
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+sudo mkdir -p $script_dir/pre-built/$target_dir
+sudo cp -rf $target/$target_dir/wheels $script_dir/pre-built/$target_dir
---
+++ sdbuild/packages/python_packages_focal/pre-built/README.md
@@ -0,0 +1,3 @@
+folder gets populated with wheels built from Python source inside a chroot (e.g. numpy for armhf)
+
+See pre.sh and post.sh in the package folder to see how files are copied to/from the sdcard image
