ef8196a7a6a93355de5a71387f76f01aae242e99
Peter Ogden <peter.ogden@cantab.net>
2021-11-11 17:37:39 +0000

    Fix Chromium and Fluxbox in 20.04 (#1308)
    * Fix Chromium and Fluxbox in 20.04
    
    * Seed the chromium snap correctly
    
    * Fixes for snap-based Chromium
    
     - Wait for the snaps to be seeded before starting chromium. Note that the snapd.seeded.service systemd target times out with a failure so we're doing it manually
     - Update the killchromium script to the new process and snaps
     - Add the required core snap to the seed
    
    * Bringup fluxbox before chromium installed

--- sdbuild/packages/x11/pre.sh
+++ sdbuild/packages/x11/pre.sh
@@ -9,6 +9,13 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo cp $script_dir/xorg.conf $target/etc/X11/
 sudo cp $script_dir/pynq-x11.service $target/lib/systemd/system
 sudo cp $script_dir/armsoc.patch $target/
+sudo cp $script_dir/pixmap.patch $target/
+
+snap known --remote model series=16 brand-id=generic model=generic-classic > /tmp/model
+
+sudo snap prepare-image --snap chromium --snap core18 --snap "gnome-3-28-1804" --snap "gtk-common-themes" --snap bare --snap core --classic --arch arm64 /tmp/model $target
+
+rm /tmp/model
 
 sudo chroot / mkdir -p $target/root/.config/midori
 sudo cp $script_dir/midori_config $target/root/.config/midori/config
@@ -19,7 +26,9 @@ sudo chroot / mkdir -p $target/home/xilinx/.config/chromium/Default
 sudo cp $script_dir/chromium_config $target/home/xilinx/.config/chromium/Default/Preferences
 sudo chroot / touch "$target/home/xilinx/.config/chromium/First Run"
 sudo cp $script_dir/killchromium $target/usr/bin
+sudo cp $script_dir/startchromium $target/usr/local/bin
+
 if [ -f $BUILD_ROOT/PYNQ/pynq/notebooks/Welcome\ to\ Pynq.ipynb ]; then
 	sudo cp -f $BUILD_ROOT/PYNQ/pynq/notebooks/Welcome\ to\ Pynq.ipynb \
-	$target/usr/local/share/x11/
+	$target/home/xilinx/
 fi
--- sdbuild/packages/x11/qemu.sh
+++ sdbuild/packages/x11/qemu.sh
@@ -5,17 +5,22 @@ set -e
 
 for f in /etc/profile.d/*.sh; do source $f; done
 
-if [ -f /usr/local/share/x11/Welcome\ to\ Pynq.ipynb ]; then
+if [ -f /home/xilinx/Welcome\ to\ Pynq.ipynb ]; then
 	jupyter nbconvert --to html \
-	/usr/local/share/x11/Welcome\ to\ Pynq.ipynb
-	rm -f /usr/local/share/x11/Welcome\ to\ Pynq.ipynb
+	/home/xilinx/Welcome\ to\ Pynq.ipynb
+	rm -f /home/xilinx/Welcome\ to\ Pynq.ipynb
 fi
 
 systemctl enable pynq-x11.service
+systemctl set-default multi-user
 
 chown -R xilinx:xilinx /home/xilinx/.config/chromium
 chmod a+x /usr/bin/killchromium
 rm -rf /home/xilinx/.config/chromium/Singleton*
+echo startfluxbox > /root/.xinitrc
+
+chown -R xilinx:xilinx /home/xilinx/.config
+runuser -l xilinx -c "xdg-settings set default-web-browser chromium-browser.desktop"
 
 mkdir /root/armsoc_build
 cd /root/armsoc_build
@@ -23,6 +28,7 @@ cd /root/armsoc_build
 git clone https://anongit.freedesktop.org/git/xorg/driver/xf86-video-armsoc.git -c http.sslverify=false
 cd xf86-video-armsoc
 git apply /armsoc.patch --ignore-whitespace
+git apply /pixmap.patch --ignore-whitespace
 ./autogen.sh
 ./configure --prefix=/usr
 make -j4
