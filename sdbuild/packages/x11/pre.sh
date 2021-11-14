18b9e6db847c855771e66bfaeaa37171a403f8d6
Peter Ogden <peter.ogden@cantab.net>
2021-11-14 18:22:51 +0000

    Move to firefox rather than Chromium for desktop browser (#1313)

--- sdbuild/packages/x11/pre.sh
+++ sdbuild/packages/x11/pre.sh
@@ -11,22 +11,11 @@ sudo cp $script_dir/pynq-x11.service $target/lib/systemd/system
 sudo cp $script_dir/armsoc.patch $target/
 sudo cp $script_dir/pixmap.patch $target/
 
-snap known --remote model series=16 brand-id=generic model=generic-classic > /tmp/model
-
-sudo snap prepare-image --snap chromium --snap core18 --snap "gnome-3-28-1804" --snap "gtk-common-themes" --snap bare --snap core --classic --arch arm64 /tmp/model $target
-
-rm /tmp/model
-
 sudo chroot / mkdir -p $target/root/.config/midori
 sudo cp $script_dir/midori_config $target/root/.config/midori/config
 sudo cp -r $script_dir/fluxbox_config $target/root/.fluxbox
 sudo chroot / mkdir -p $target/usr/local/share/x11
 sudo cp $script_dir/pynq-background.png $target/usr/local/share/x11
-sudo chroot / mkdir -p $target/home/xilinx/.config/chromium/Default
-sudo cp $script_dir/chromium_config $target/home/xilinx/.config/chromium/Default/Preferences
-sudo chroot / touch "$target/home/xilinx/.config/chromium/First Run"
-sudo cp $script_dir/killchromium $target/usr/bin
-sudo cp $script_dir/startchromium $target/usr/local/bin
 
 if [ -f $BUILD_ROOT/PYNQ/pynq/notebooks/Welcome\ to\ Pynq.ipynb ]; then
 	sudo cp -f $BUILD_ROOT/PYNQ/pynq/notebooks/Welcome\ to\ Pynq.ipynb \
