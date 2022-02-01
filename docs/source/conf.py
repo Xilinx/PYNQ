2712f774888043634c456a6612f22a87c433d8df
Mario Ruiz <mruiznog@xilinx.com>
2022-02-01 12:56:24 +0000

    Add XRT to the mock modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['_video', 'pynq.lib._video']
+MOCK_MODULES = ['_video', 'pynq.lib._video', 'pynq._3rdparty.xrt']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
