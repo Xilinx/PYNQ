08571f6a6ffc19b4e5f729389dfcb5fa6b9875c3
Mario Ruiz <mruiznog@xilinx.com>
2022-06-17 12:43:59 +0100

    Update test cases

--- tests/test_mmio.py
+++ tests/test_mmio.py
@@ -1,7 +1,8 @@
-import warnings
-import struct
-import pytest
+import numpy as np
 import pynq
+import pytest
+import struct
+import warnings
 
 
 from .mock_devices import MockDeviceBase, MockRegisterDevice
@@ -22,6 +23,21 @@ TEST_READ_DATA = [
     (BASE_ADDRESS + 8, struct.pack('I', 0xbeefcafe))
 ]
 
+TEST_DATA_NUMPY = [
+    (300, np.uint32(53970361), int(53970361).to_bytes(4, 'little')),
+    (256, -14921033, int(np.uint32(-14921033)).to_bytes(4, 'little')),
+    (248, np.uint16(12045), np.uint16(12045).tobytes()),
+    (260, np.int16(-12045), np.int16(-12045).tobytes()),
+    (260, np.uint8(248), np.uint8(248).tobytes()),
+    (200, np.int8(-99), np.int8(-99).tobytes()),
+    (260, np.uint64(4181616581), np.uint64(4181616581).tobytes()),
+    (200, np.int64(-14103463169), np.int64(-14103463169).tobytes()),
+]
+
+TEST_DATA_FLOAT = [
+    (8, 98.576, int(0x42c526e9).to_bytes(4, 'little')),
+    (48, np.single(-32.587), int(0xc2025917).to_bytes(4, 'little'))
+]
 
 @pytest.fixture
 def register_device():
@@ -129,11 +145,40 @@ def test_unaligned_offset_read(device):
         'Unaligned read: offset must be multiple of 4.'
 
 
-def test_float_write(device):
+@pytest.mark.parametrize('transaction', TEST_DATA_NUMPY)
+def test_reg_write_numpy(transaction, register_device):
+    offset, value, bytesobj = transaction
+    device = register_device
     mmio = pynq.MMIO(BASE_ADDRESS, ADDR_RANGE, device=device)
+    with device.check_transactions([], [(BASE_ADDRESS+offset, bytesobj)]):
+        mmio.write(offset, value)
+
+
+@pytest.mark.parametrize('transaction', TEST_DATA_FLOAT)
+def test_reg_write_float(transaction, register_device):
+    offset, pyobj, bytesobj = transaction
+    device = register_device
+    mmio = pynq.MMIO(BASE_ADDRESS, ADDR_RANGE, device=device)
+    with device.check_transactions([], [(BASE_ADDRESS+offset, bytesobj)]):
+        mmio.write(offset, pyobj)
+
+
+def test_reg_read_float(mmap_device):
+    device = mmap_device
+    mmio = pynq.MMIO(BASE_ADDRESS, ADDR_RANGE, device=device)
+    offset, testdata = (956, np.float32(102.687))
+    mmio.write(offset, testdata)
+    read = mmio.read(offset, dtype=type(testdata))
+    assert np.isclose(read, testdata, rtol=1e-05, atol=1e-08, equal_nan=False)
+
+
+def test_reg_write_unsupported_type(mmap_device):
+    device = mmap_device
+    mmio = pynq.MMIO(BASE_ADDRESS, ADDR_RANGE, device=device)
+    value = np.half(20.22)
     with pytest.raises(ValueError) as excinfo:
-        mmio.write(4, 8.5)
-    assert str(excinfo.value) == "Data type must be int or bytes."
+        mmio.write(4, value)
+    assert str(excinfo.value) == "dtype \'{}\' is not supported".format(type(value))
 
 
 def test_oob_write(device):
@@ -159,3 +204,27 @@ def test_deprecated_debug_keyword(mmap_device):
             'Warning is not of type DeprecationWarning'
         assert "debug" in str(w[-1].message), \
             'Warning not related to keyword debug'
+
+def test_deprecated_length_keyword(mmap_device):
+    device = mmap_device
+    mmio = pynq.MMIO(BASE_ADDRESS, ADDR_RANGE, device=device)
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("always")
+        read = mmio.read(4, length=1)
+        assert len(w) == 1, 'No warnings when providing keyword debug'
+        assert issubclass(w[-1].category, DeprecationWarning), \
+            'Warning is not of type DeprecationWarning'
+        assert "length" in str(w[-1].message), \
+            'Keyword length has been deprecated.'
+
+def test_deprecated_word_order_keyword(mmap_device):
+    device = mmap_device
+    mmio = pynq.MMIO(BASE_ADDRESS, ADDR_RANGE, device=device)
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("always")
+        read = mmio.read(4, word_order='little')
+        assert len(w) == 1, 'No warnings when providing keyword debug'
+        assert issubclass(w[-1].category, DeprecationWarning), \
+            'Warning is not of type DeprecationWarning'
+        assert "word_order" in str(w[-1].message), \
+            'Keyword word_order has been deprecated.'
