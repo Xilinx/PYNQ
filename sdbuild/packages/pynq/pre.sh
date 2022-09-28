e1bb28fda67e978802b873d9e164d50a51d8f6cd
schelleg <graham.schelle@gmail.com>
2022-09-28 17:45:02 +0100

    SDBuild and pynq packaging simplifications
    * Pynq sdbuild will no longer discover and build bitstreams for 3rd party board sdcard builds.
    All 3rd party board repositories should now build their own overlay bitstreams before calling
    pynq’s sdbuild/Makefile.  Please see PYNQ-ZU or RFSoC-PYNQ repositories for examples of
    board repository Makefiles and their interaction with pynq/sdbuild.
    
    * Pynq packaging (i.e., creation of dist/pynq_sdist.*.tar.gz) is simplified as a Makefile (build.mk) -
    by default, sdcard building for 3rd party boards will expect a pynq source distribution
    already built and will not rebuild that source distribution to include 3rd party overlays
    and notebooks.  Now 3rd party board overlays and notebooks are delivered in sdbuild/packages/pynq/pre.sh.
    
    * Prebuilt rootfilesystems and pynq source distribution are now expected at sdbuild/prebuilt/ with names
    pynq_rootfs.aarch64.tar.gz, pynq_rootfs.arm.tar.gz and pynq_sdist.tar.gz.  The Makefile has useful
    warning messages when it cannot find prebuilts at the expected location.
    
    Both simplifications are API changes but will simplify 3rd party board builds.  SD card building
    should be greatly sped up as pynq source distribution and architecture specific root
    filesystems should never be rebuilt outside of the core PYNQ team.

--- sdbuild/packages/pynq/pre.sh
+++ sdbuild/packages/pynq/pre.sh
@@ -4,22 +4,20 @@ set -x
 set -e
 
 target=$1
+pynqoverlays_dir=$target/usr/local/share/pynq-venv/lib/python3.10/site-packages/pynq/overlays
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# copy files from staging area to the qemu environment
-sudo mkdir -p $target/home/xilinx/pynq_git/boards
+# 1. Copy files from staging area to the qemu environment
 sudo mkdir -p $target/home/xilinx/pynq_git/dist
 
-sudo cp $script_dir/pl_server.sh $target/usr/local/bin
-sudo cp $script_dir/pl_server.service $target/lib/systemd/system
 sudo cp $script_dir/pynq_hostname.sh $target/usr/local/bin
 sudo cp $script_dir/boardname.sh $target/etc/profile.d
 
-# add revision file
+
+# 2. Create and copy the REVISION file
 echo "Release $(date +'%Y_%m_%d') $(git rev-parse --short=7 --verify HEAD)" \
 	> $BUILD_ROOT/PYNQ/REVISION
 
-# copy external board files into the staging area
 if [ ${PYNQ_BOARD} != "Unknown" ]; then
 	cd ${PYNQ_BOARDDIR}
 	if git tag > /dev/null 2>&1 && [ $? -eq 0 ]; then
@@ -28,64 +26,52 @@ if [ ${PYNQ_BOARD} != "Unknown" ]; then
 			"$(git config --get remote.origin.url)"\
 			>> $BUILD_ROOT/PYNQ/REVISION
 	fi
-	if [ ! -d "$BUILD_ROOT/PYNQ/boards/${PYNQ_BOARD}" ]; then
-		cp -rf ${PYNQ_BOARDDIR} $BUILD_ROOT/PYNQ/boards/${PYNQ_BOARD}
-	fi
 fi
 
-# move revision onto target
 sudo cp -rf $BUILD_ROOT/PYNQ/REVISION $target/home/xilinx/REVISION
 sudo cp -rf $BUILD_ROOT/PYNQ/REVISION $target/boot/REVISION
-
 if [ -d /usr/local/share/fatfs_contents ]; then
     sudo cp -rf $BUILD_ROOT/PYNQ/REVISION /usr/local/share/fatfs_contents/REVISION
 fi
 
-if [ -n "$PYNQ_SDIST" ] && [ "$BOARDDIR" -ef "$DEFAULT_BOARDDIR" ]; then
-	# using prebuilt sdist with non-external board
-	# nothing to do except copy sdist to target folder
-	sudo cp ${PYNQ_SDIST} $target/home/xilinx/pynq_git/dist
-else
-	if [ -n "$PYNQ_SDIST" ] && [ ! "$BOARDDIR" -ef "$DEFAULT_BOARDDIR" ]; then
-		# using prebuilt sdist with external board
-		# copy bitstreams, microblaze bsps and binaries from sdist
-		sdist_untar=$BUILD_ROOT/PYNQ/sdist_untar
-		mkdir -p ${sdist_untar}
-		tar -xzvf ${PYNQ_SDIST} -C ${sdist_untar}
-		cd $BUILD_ROOT/PYNQ
-		rsync -rptL --ignore-existing ${sdist_untar}/*/pynq/lib/* pynq/lib/
-		boards=`ls ${sdist_untar}/*/boards`
-		for bd_name in $boards ; do
-			cd ${sdist_untar}/*/boards/${bd_name}
-			overlays=`find . -maxdepth 2 -iname '*.bit' -printf '%h\n' | cut -f2 -d"/"`
-			for ol in $overlays ; do
-				sudo cp -fL $ol/*.bit $ol/*.hwh \
-					$BUILD_ROOT/PYNQ/boards/${bd_name}/$ol 2>/dev/null||:
-			done
-		done
-		rm -rf ${sdist_untar}
+
+# 3. 3rd party board may have additional pynq.overlay entries and notebooks - deliver those now if they exist
+if [ "$BOARDDIR" != "$DEFAULT_BOARDDIR" ] && [ "$PYNQ_BOARD" != "Unknown" ]; then
+
+    overlays=`find $BOARDDIR/$PYNQ_BOARD -maxdepth 2 -iname '*.bit' -printf '%h\n'`
+    for ol in $overlays ; do
+	ol_name=`basename $ol`
+	sudo mkdir -p $pynqoverlays_dir/$ol_name
+	sudo cp -fL $ol/*.bit $ol/*.hwh $ol/*.py $pynqoverlays_dir/$ol_name
+	
+	if [ -e $ol_name/notebooks ]; then
+		sudo mkdir -p $target/home/xilinx/pynq_git/notebooks/$ol_name
+		sudo cp -fLr $ol_name/notebooks/* $target/home/xilinx/pynq_git/notebooks/$ol_name
 	fi
-	# build bitstream, microblazes' bsps and binaries
-	cd $BUILD_ROOT/PYNQ
-	./build.sh
-	# get rid of Vivado temp files in case there are any
-	boards=`find boards -maxdepth 2 -name '*.spec' -printf '%h\n' | cut -f2 -d"/"`
-	for bd_name in $boards ; do
-		cd $BUILD_ROOT/PYNQ/boards/${bd_name}
-		overlays=`find . -maxdepth 2 -iname 'makefile' -printf '%h\n' | cut -f2 -d"/"`
-		for ol in $overlays ; do
-			cd $BUILD_ROOT/PYNQ/boards/${bd_name}/$ol
-			make clean
-			if [[ -e $ol.bit.link ]]; then
-				rm -f $ol.bit
-			fi
-			if [[ -e $ol.hwh.link ]]; then
-				rm -f $ol.hwh
-			fi
-		done
-	done
-	# create sdist
-	cd $BUILD_ROOT/PYNQ
-	python3 setup.py sdist
-	sudo cp dist/*.tar.gz $target/home/xilinx/pynq_git/dist
+    done
+    
+    if [ -e $BOARDDIR/$PYNQ_BOARD/notebooks ]; then
+    	sudo mkdir -p $target/home/xilinx/pynq_git/notebooks
+	sudo cp -fLr $BOARDDIR/$PYNQ_BOARD/notebooks/* $target/home/xilinx/pynq_git/notebooks
+    fi    
+fi
+
+# 4. Deliver PYNQ python source distribution for installation
+if [ -n "$REBUILD_PYNQ_SDIST" ]; then
+    echo "
+
+    Warning ... setting REBUILD_PYNQ_SDIST will result in several bitstream and bsp builds
+                      please rerun `Make` without REBUILD_PYNQ_SDIST set using latest PYNQ source
+                      distribution on PYPI at: https://pypi.org/project/pynq/#files
+    "
+    sleep 10
+
+    # build bitstream, microblazes' bsps and binaries
+    cd $BUILD_ROOT/PYNQ
+    make -f build.mk
+    sudo cp dist/*.tar.gz $target/home/xilinx/pynq_git/dist
+else
+    # using prebuilt sdist with non-external board
+    # nothing to do except copy sdist to target folder
+    sudo cp ${PYNQ_SDIST} $target/home/xilinx/pynq_git/dist	
 fi
