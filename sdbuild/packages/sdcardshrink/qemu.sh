e8cb4d1b2e6a86932e87a5ff3b375ac931090d1e
skalade <sarunask@xilinx.com>
2022-10-05 20:54:49 +0100

    remove extra files from firmware

--- sdbuild/packages/sdcardshrink/qemu.sh
+++ sdbuild/packages/sdcardshrink/qemu.sh
@@ -3,4 +3,4 @@ set -x
 
 # remove unnecessary firmware to save space for zynq7000 boards
 cd /lib/firmware
-rm -rf netronome mellanox mrvl qcom intel liquidio
+rm -rf netronome mellanox mrvl qcom intel liquidio amdgpu i915 dpaa2
