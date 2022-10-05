ec566bb57741616016edebb301bf4cc4a53ab668
skalade <sarunask@xilinx.com>
2022-10-05 14:31:17 +0100

    remove sudo

--- sdbuild/packages/sdcardshrink/qemu.sh
+++ sdbuild/packages/sdcardshrink/qemu.sh
@@ -3,4 +3,4 @@ set -x
 
 # remove unnecessary firmware to save space for zynq7000 boards
 cd /lib/firmware
-sudo rm -rf netronome mellanox mrvl qcom intel liquidio
+rm -rf netronome mellanox mrvl qcom intel liquidio
