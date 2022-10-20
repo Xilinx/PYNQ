37170d0823424a2f478761fd491f48e8d0c528fc
sfleming <sfleming@xilinx.com>
2022-10-20 16:57:19 +0100

    increasing version to 3.0.1

--- pynq/__init__.py
+++ pynq/__init__.py
@@ -17,7 +17,7 @@ from .registers import Register
 from .uio import UioController
 
 __all__ = ["lib", "tests"]
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 # This ID will always be tied to a specific release tag
 # since the file will be modified to edit the version
 __git_id__ = "$Id$"
