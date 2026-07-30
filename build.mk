e07c3b96503e519970551037c487726815da910b
josh goldsmith <josh.goldsmith@amd.com>
2026-07-30 14:20:03 +0100

    docs, build.mk: drop Zynq-7000 references; bump version to 4.0.0

--- build.mk
+++ build.mk
@@ -3,48 +3,30 @@
 
 # Builds final pynq source distribution with overlays and BSPs included
 
-VERSION := 3.1.3
+VERSION := 4.0.0
 SDIST := dist/pynq-$(VERSION).tar.gz
 
-BITS := boards/Pynq-Z1/logictools/logictools.bit \
-	boards/Pynq-Z2/logictools/logictools.bit \
-	boards/Pynq-Z1/base/base.bit \
-	boards/Pynq-Z2/base/base.bit \
-	boards/ZCU104/base/base.bit 
+BITS := boards/ZCU104/base/base.bit
 
-LOGICTOOLS_BSP := pynq/lib/logictools/bsp_lcp_ar_mb/lscript.ld
-BASE_BSP := pynq/lib/rpi/bsp_iop_rpi/lscript.ld
+BASE_BSP := pynq/lib/pmod/bsp_iop_pmod/iop_pmoda_mb/lib/libxil.a
 
 
-all: gitsubmodule $(BITS) $(BASE_BSP) $(LOGICTOOLS_BSP) $(SDIST)
+all: gitsubmodule $(BITS) $(BASE_BSP) $(SDIST)
 	echo "Build completed: $(SDIST)"
 
 gitsubmodule:
 	git submodule update
 
-%.bit: %.tcl 
+%.bit: %.tcl
 	cd $(dir $@) ; make clean all
 
-$(LOGICTOOLS_BSP): 
-	cd boards/sw_repo ; make clean ; make XSA=../Pynq-Z2/logictools/logictools.xsa
-	rm -rf boards/sw_repo/*/*/*/*/*/code
-	rm -rf boards/sw_repo/*/*/*/*/*/libsrc
-
-	cp -rf boards/sw_repo/bsp_lcp_ar_mb/lcp_ar_mb/standalone_domain/bsp pynq/lib/logictools/bsp_lcp_ar_mb
-
-	cd pynq/lib/logictools && make && make clean
-	cd boards/sw_repo && make clean
-
 $(BASE_BSP):
-	cd boards/sw_repo ; make clean ; make XSA=../Pynq-Z2/base/base.xsa
+	cd boards/sw_repo ; make clean ; make XSA=../ZCU104/base/base.xsa
 	rm -rf boards/sw_repo/*/*/*/*/*/code
 	rm -rf boards/sw_repo/*/*/*/*/*/libsrc
+	cp -rf boards/sw_repo/bsp_iop_pmod0_mb/iop_pmod0_mb/standalone_domain/bsp pynq/lib/pmod/bsp_iop_pmod
+	mv pynq/lib/pmod/bsp_iop_pmod/iop_pmod0_mb pynq/lib/pmod/bsp_iop_pmod/iop_pmoda_mb
 
-	cp -rf boards/sw_repo/bsp_iop_arduino_mb/iop_arduino_mb/standalone_domain/bsp pynq/lib/arduino/bsp_iop_arduino
-	cp -rf boards/sw_repo/bsp_iop_pmoda_mb/iop_pmoda_mb/standalone_domain/bsp pynq/lib/pmod/bsp_iop_pmod
-	cp -rf boards/sw_repo/bsp_iop_rpi_mb/iop_rpi_mb/standalone_domain/bsp pynq/lib/rpi/bsp_iop_rpi
-
-	cd pynq/lib/arduino && make && make clean
 	cd pynq/lib/pmod && make && make clean
 	cd boards/sw_repo && make clean
 
