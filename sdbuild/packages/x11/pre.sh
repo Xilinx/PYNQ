4d1dda69668aa099e0242ef1b093522630af4c2b
josh goldsmith <josh.goldsmith@amd.com>
2026-08-26 17:47:36 +0100

    Switch Noble X stack from armsoc to modesetting for EDF DRM

--- sdbuild/packages/x11/pre.sh
+++ sdbuild/packages/x11/pre.sh
@@ -8,8 +8,6 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo cp $script_dir/xorg.conf $target/etc/X11/
 sudo cp $script_dir/pynq-x11.service $target/lib/systemd/system
-sudo cp $script_dir/armsoc.patch $target/
-sudo cp $script_dir/pixmap.patch $target/
 
 sudo chroot / mkdir -p $target/root/.config/midori
 sudo cp $script_dir/midori_config $target/root/.config/midori/config
